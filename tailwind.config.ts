4c3969294d9a95296136051ef93d31f199209139
heonq <gkagjsrb@naver.com>
2025-02-28 10:05:09 +0900

    chore: tailwindcss/forms 설치

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import forms from '@tailwindcss/forms';
 
 const config: Config = {
   content: [
@@ -85,6 +86,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [forms],
 };
 export default config;
