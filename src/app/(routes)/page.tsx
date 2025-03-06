300062b39e8e13277f4984666385c93354fae2aa
Jinhan <wlsgks333@naver.com>
2025-03-06 15:50:01 +0900

    style: 렌딩페이지 소개 텍스트 스타일 적용

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <div>main페이지 입니다</div>;
+  return (
+    <div className='w-full min-h-screen bg-[var(--background-400)] flex flex-col items-center'>
+      <h1 className='animate-fadeIn w-[439px] font-semibold text-[36px] leading-[50px] mt-20 text-[var(--black-500)] text-center'>
+        원하는 이사 서비스를 요청하고 견적을 받아보세요
+      </h1>
+    </div>
+  );
 }
