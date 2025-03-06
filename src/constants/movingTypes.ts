689f357874bef2e743fc7728b10e34feb03a85be
heonq <gkagjsrb@naver.com>
2025-03-06 15:52:04 +0900

    feat: movingTypes 상수 생성

---
+++ src/constants/movingTypes.ts
@@ -0,0 +1,8 @@
+export const MOVING_TYPES = {
+  small: { key: 'small', value: '소형이사' },
+  office: { key: 'office', value: '사무실이사' },
+  home: { key: 'home', value: '가정이사' },
+  custom: { key: 'custom', value: '지정 견적 요청' },
+  confirmedQuotes: { key: 'confirmedQuotes', value: '확정 견적' },
+  pendingQuote: { key: 'pendingQuote', value: '견적 대기' },
+} as const;
