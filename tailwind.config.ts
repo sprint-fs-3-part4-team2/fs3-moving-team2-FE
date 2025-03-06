a7e9f83a7a28f6af133a46f53aa73bbaab86d58e
Jinhan <wlsgks333@naver.com>
2025-03-06 15:47:18 +0900

    chore: keyframes, animation 설정 추가가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -88,6 +88,17 @@ const config: Config = {
         md: '744px',
         xl: '1200px',
       },
+
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
+
+      animation: {
+        fadeIn: 'fadeIn 2.5s ease-in-out forwards',
+      },
     },
   },
   plugins: [forms],
