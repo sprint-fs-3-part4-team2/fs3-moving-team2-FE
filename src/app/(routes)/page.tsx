55260e619c916e03c6c1b9d74d79370b6751a77d
Jinhan <wlsgks333@naver.com>
2025-03-08 01:57:27 +0900

    style: landing page media query 적용

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,61 +1,52 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 export default function Home(): JSX.Element {
   return (
     <div className='animate-fadeIn w-[439px] w-full min-h-screen bg-[var(--background-400)] flex flex-col items-center'>
-      <h1 className='animate-fadeIn w-[439px] font-semibold text-[36px] leading-[50px] mt-20 text-[var(--black-500)] text-center'>
+      <h1 className='text-[27px] xl:text-[36px] w-[325px] xl:w-[440px] font-semibold leading-[50px] mt-20 text-[var(--black-500)] text-center animate-fadeIn'>
         원하는 이사 서비스를 요청하고 견적을 받아보세요
       </h1>
 
       {/* Main Grid */}
-      <div className='mt-[40px] mb-[100px] grid grid-cols-3 gap-[24px] cursor-pointer'>
+      <div className='mt-[40px] mb-[100px] grid grid-cols-1 gap-[36px] auto-rows-fr xxl:grid-cols-3 gap-[24px] cursor-pointer'>
         {/* 세로 박스 */}
+        {/* <Link> */}
         <div
-          className={`w-[432px] h-[589px] col-span-1 row-span-2 bg-[var(--primary-blue-100)] ${landingPageCardsCommonStyles}`}
+          className={`w-[327px] h-[240px] xxl:w-[432px] xxl:h-[589px] xxl:col-span-1 xxl:row-span-2 bg-[var(--primary-blue-100)] ${landingPageCardsCommonStyles}`}
         >
           {/* Text Box */}
-          <div className='flex flex-col justify-center mt-[45px] ml-[42px]'>
-            <h2 className='font-semibold text-[30px]'>소형이사</h2>
-            <h3 className='text-[20px] text-[var(--grayscale-400)] mt-[9px]'>
+          <div className={textBoxStyles}>
+            <h2 className={textBoxHeadTitleStyles}>소형이사</h2>
+            <h3 className={textBoxHeadSubTitleStyles}>
               원룸, 투룸, 20평대 미만
             </h3>
           </div>
 
-          <div
-            style={
-              {
-                position: 'relative',
-                width: '380px',
-                height: '320px',
-              } as React.CSSProperties
-            }
-            className={firstCardImageStyles}
-          >
+          <div className={firstCardImageStyles}>
             <Image
               fill
               src='/img/landing/lundury-img.svg'
               alt='소형이사 이미지'
-              style={{
-                objectFit: 'cover',
-              } as React.CSSProperties}
+              className='object-cover'
             />
           </div>
         </div>
+        {/* </Link> */}
 
         {/* 가로 박스1 */}
+        {/* <Link> */}
         <div
-          className={`col-span-2 rounded-[32px]  bg-[var(--background)] ${landingPageCardsCommonStyles}`}
+          className={`xxl:col-span-2 rounded-[32px]  bg-[var(--background)] ${landingPageCardsCommonStyles}`}
         >
           {/* Text Box */}
-          <div className='relative flex flex-col justify-center mt-[45px] ml-[42px]'>
-            <h2 className='font-semibold text-[30px]'>가정이사</h2>
-            <h3 className='text-[20px] text-[var(--grayscale-400)] mt-[9px]'>
-              쓰리룸, 20평대 미만
-            </h3>
+          <div className={textBoxStyles}>
+            <h2 className={textBoxHeadTitleStyles}>가정이사</h2>
+            <h3 className={textBoxHeadSubTitleStyles}>쓰리룸, 20평대 미만</h3>
           </div>
 
-          <div className='absolute w-[460px] h-[260px] left-[450px] bottom-[-8px] animate-moveForward'>
+          <div className='absolute w-[250px] h-[140px] left-[98px] bottom-[-5px] xxl:w-[460px] xxl:h-[260px] xxl:left-[465px] xxl:bottom-[-8px] animate-moveForward'>
             <Image
               fill
               src='/img/landing/car-img.svg'
@@ -64,20 +55,20 @@ export default function Home(): JSX.Element {
             />
           </div>
         </div>
+        {/* </Link> */}
 
         {/* 가로 박스2 */}
+        {/* <Link> */}
         <div
-          className={`col-span-2 rounded-[32px]  bg-[var(--background)] ${landingPageCardsCommonStyles}`}
+          className={`xxl:col-span-2 rounded-[32px]  bg-[var(--background)] ${landingPageCardsCommonStyles}`}
         >
           {/* Text Box */}
-          <div className='flex flex-col justify-center mt-[45px] ml-[42px]'>
-            <h2 className='font-semibold text-[30px]'>기업, 사무실 이사</h2>
-            <h3 className='text-[20px] text-[var(--grayscale-400)] mt-[9px]'>
-              사무실, 상업공간
-            </h3>
+          <div className={textBoxStyles}>
+            <h2 className={textBoxHeadTitleStyles}>기업, 사무실 이사</h2>
+            <h3 className={textBoxHeadSubTitleStyles}>사무실, 상업공간</h3>
           </div>
 
-          <div className='absolute w-[570px] h-[240px] left-[320px] bottom-[0px] animate-enlargeAndShake'>
+          <div className='absolute w-[280px] h-[130px] left-[50px] bottom-[0px] xxl:w-[570px] xxl:h-[240px] xxl:left-[320px] xxl:bottom-[0px] animate-enlargeAndShake'>
             <Image
               fill
               src='/img/landing/building-img.svg'
@@ -86,6 +77,7 @@ export default function Home(): JSX.Element {
             />
           </div>
         </div>
+        {/* </Link> */}
       </div>
     </div>
   );
@@ -95,4 +87,12 @@ const landingPageCardsCommonStyles =
   'transform transition-all duration-500 hover:scale-[1.03] hover:shadow-[4px_4px_8px_-1px_rgba(0,0,0,0.2)] active:shadow-[inset_2px_2px_5px_2px_rgba(0,0,0,0.2)] active:scale-[1] rounded-[32px] shadow-[3px_3px_5px_0px_rgba(0,0,0,0.1)] overflow-hidden group';
 
 const firstCardImageStyles =
-  'animate-shakeVerticalWithAngle transition-transform duration-500 group-hover:-translate-y-2 group-active:translate-y-2" top-[110px] left-[83px] transform transition-all duration-500 group-hover:-translate-y-5 group-active:translate-y-2 animate-moveDown group-active:width-[300px] group-active:height-[290px]'
+  'w-[196px] h-[164px] bottom-[6px] right-[0px] xxl:w-[380px] xxl:h-[320px] xxl:bottom-[50px] xxl:left-[83px] animate-shakeVerticalWithAngle transition-transform duration-500 group-hover:-translate-y-2 group-active:translate-y-2" transform transition-all duration-500 group-hover:-translate-y-5 group-active:translate-y-2 animate-moveDown group-active:width-[300px] group-active:height-[290px] absolute';
+
+const textBoxStyles =
+  'flex flex-col justify-center mt-[20px] ml-[30px] xxl:mt-[45px] xxl:ml-[42px]';
+
+const textBoxHeadTitleStyles = 'font-semibold text-[25px] xxl:text-[30px]';
+
+const textBoxHeadSubTitleStyles =
+  'text-[16px] xxl:text-[20px] text-[var(--grayscale-400)] mt-[5px] xxl:mt-[9px]';
