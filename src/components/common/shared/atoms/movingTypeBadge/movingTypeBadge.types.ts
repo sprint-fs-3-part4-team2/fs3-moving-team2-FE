5fcd7c39d9ce74b8f24226fca3e93fc6d820db1c
heonq <gkagjsrb@naver.com>
2025-03-09 20:32:44 +0900

    feat: MovingTypeBadge 컴포넌트 수정

--- src/components/common/shared/atoms/movingTypeBadge/movingTypeBadge.types.ts
+++ src/components/common/shared/atoms/movingTypeBadge/movingTypeBadge.types.ts
@@ -1,8 +1,13 @@
-import { MOVING_TYPES } from '@/constants/movingTypes';
+import { MOVING_STATE, MOVING_TYPES } from '@/constants/movingTypes';
 
-type MovingType = (typeof MOVING_TYPES)[keyof typeof MOVING_TYPES]['key'];
+export type MovingTypes =
+  (typeof MOVING_TYPES)[keyof typeof MOVING_TYPES]['key'];
+export type MovingStates =
+  (typeof MOVING_STATE)[keyof typeof MOVING_STATE]['key'];
+
+export type MovingInfo = MovingTypes | MovingStates;
 
 export interface MovingTypeBadgeProps {
-  type: MovingType;
+  type: MovingInfo;
   className?: string;
 }
