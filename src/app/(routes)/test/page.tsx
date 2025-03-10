e58ef73fe7b2c32b87ce3544dae1a456fc666d68
heziss <heziss0965@gmail.com>
2025-03-10 10:37:38 +0900

    fix: 프롭스를 받아오는 방식으로 변경

---
+++ src/app/(routes)/test/page.tsx
@@ -0,0 +1,9 @@
+import ShareButtons from '@/components/ShareButtons';
+
+export default function TestPage() {
+  return (
+    <div className='flex-col ml-10 flex justify-center h-screen'>
+      <ShareButtons text='나만 알기엔 아쉬운 기사님인가요?' />
+    </div>
+  );
+}
--- src/components/ShareButtons.tsx
+++ src/components/ShareButtons.tsx
@@ -2,7 +2,11 @@
 
 import Image from 'next/image';
 
-const ShareButtons = () => {
+interface ShareButtonsProps {
+  text: string;
+}
+
+const ShareButtons = ({ text }: ShareButtonsProps) => {
   const currentUrl = typeof window !== 'undefined' ? window.location.href : '';
 
   // 링크 공유
@@ -17,7 +21,7 @@ const ShareButtons = () => {
 
   // 카카오 공유
   const shareOnKakao = () => {
-    const kakaoUrl = `https://sharer.kakao.com/talk/friends/picker/link?app_key=YOUR_KAKAO_APP_KEY&target_id_type=URL&target_id=${encodeURIComponent(currentUrl)}`;
+    const kakaoUrl = `https://sharer.kakao.com/talk/friends/picker/link?app_key=e37ad5eb9d777fdc772bfec0c290ddcd&target_id_type=URL&target_id=${encodeURIComponent(currentUrl)}`;
     window.open(kakaoUrl, '_blank', 'width=600,height=600');
   };
 
@@ -28,47 +32,56 @@ const ShareButtons = () => {
   };
 
   return (
-    <div className='flex gap-4 items-center'>
-      {/* 링크 복사 */}
-      <button
-        onClick={handleCopyLink}
-        className='flex items-center justify-center rounded-2xl border-[1px] border-grayscale-200 bg-grayscale-50 w-16 h-16'
-      >
-        <Image
-          src='/icons/share/clip.svg'
-          alt='링크 공유'
-          width={36}
-          height={36}
-        />
-      </button>
+    <div>
+      <p className='text-xl font-semibold mb-5'>{text}</p>
 
-      {/* 카카오 공유 */}
-      <button
-        onClick={shareOnKakao}
-        className='flex items-center justify-center rounded-2xl bg-[#FAE100] w-16 h-16'
-      >
-        <Image
-          src='/icons/share/kakao.svg'
-          alt='카카오톡 공유'
-          width={28}
-          height={28}
-        />
-      </button>
+      <div className='flex gap-4 items-center'>
+        {/* 링크 복사 */}
+        <button
+          onClick={handleCopyLink}
+          className='flex items-center justify-center rounded-2xl border-[1px] border-grayscale-200 bg-grayscale-50 w-16 h-16'
+        >
+          <Image
+            src='/icons/share/clip.svg'
+            alt='링크 공유'
+            width={36}
+            height={36}
+          />
+        </button>
 
-      {/* 페이스북 공유 */}
-      <button
-        onClick={shareOnFacebook}
-        className='flex items-center justify-center rounded-2xl bg-[#4285F4] w-16 h-16'
-      >
-        <Image
-          src='/icons/share/facebook.svg'
-          alt='페이스북 공유'
-          width={14}
-          height={27}
-        />
-      </button>
+        {/* 카카오 공유 */}
+        <button
+          onClick={shareOnKakao}
+          className='flex items-center justify-center rounded-2xl bg-[#FAE100] w-16 h-16'
+        >
+          <Image
+            src='/icons/share/kakao.svg'
+            alt='카카오톡 공유'
+            width={28}
+            height={28}
+          />
+        </button>
+
+        {/* 페이스북 공유 */}
+        <button
+          onClick={shareOnFacebook}
+          className='flex items-center justify-center rounded-2xl bg-[#4285F4] w-16 h-16'
+        >
+          <Image
+            src='/icons/share/facebook.svg'
+            alt='페이스북 공유'
+            width={14}
+            height={27}
+          />
+        </button>
+      </div>
     </div>
   );
 };
 
 export default ShareButtons;
+
+/* 
+예시
+<ShareButtons text="나만 알기엔 아쉬운 기사님인가요?"/>
+*/
