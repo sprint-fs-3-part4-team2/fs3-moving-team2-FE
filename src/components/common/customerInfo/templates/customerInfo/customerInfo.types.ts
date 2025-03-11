8f2ffcf17bb98b0c7ed892ff32cb498fd46d1ae1
heonq <gkagjsrb@naver.com>
2025-03-11 14:09:29 +0900

    feat: CustomerInfo 컴포넌트 구현

---
+++ src/components/common/customerInfo/templates/customerInfo/customerInfo.types.ts
@@ -0,0 +1,28 @@
+import {
+  MovingStates,
+  MovingTypes,
+} from '@/components/common/shared/atoms/movingTypeBadge/movingTypeBadge.types';
+
+export interface CustomerInfoBaseProps {
+  movingType: MovingTypes;
+  quoteState: MovingStates;
+  isCustomQuote: boolean;
+  customerName: string;
+  movingDate: Date;
+  departure: string;
+  arrival: string;
+}
+
+export interface RequestedQuoteProps extends CustomerInfoBaseProps {
+  variant: 'requested';
+  onSubmit: () => void;
+  onDecline: () => void;
+  requestedAt: Date;
+}
+
+export interface SubmittedQuoteProps extends CustomerInfoBaseProps {
+  variant: 'submitted';
+  quotePrice?: number;
+}
+
+export type CustomerInfoProps = RequestedQuoteProps | SubmittedQuoteProps;
---
+++ src/components/common/customerInfo/templates/customerInfo/index.tsx
@@ -0,0 +1,41 @@
+import MovingInfo from '@/components/common/moverInfo/organisms/movingInfo';
+import MovingTypeGroup from '../../../shared/molecules/movingTypeGroup';
+import CustomerName from '../../atoms/customerName';
+import HorizontalDivider from '../../atoms/horizontalDivider';
+import { CustomerInfoProps } from './customerInfo.types';
+import RequestedQuoteButtons from '../../molecules/requestedQuoteButtons';
+import QuotePriceForList from '@/components/common/shared/atoms/quotePriceForList';
+
+export default function CustomerInfo(props: CustomerInfoProps) {
+  return (
+    <div className='flex flex-col gap-4 shadow-primary px-4 md:px-4 xl:px-6 py-4 md:py-4 xl:py-5 rounded-[16px]'>
+      <div className='flex justify-between'>
+        <MovingTypeGroup
+          isCustomQuote={props.isCustomQuote}
+          movingType={props.movingType}
+          quoteState={props.quoteState}
+        />
+      </div>
+      <CustomerName name={props.customerName} />
+      <HorizontalDivider />
+      <MovingInfo
+        movingDate={props.movingDate}
+        departure={props.departure}
+        arrival={props.arrival}
+        showDayOfWeek={true}
+      />
+      {props.variant === 'requested' && (
+        <RequestedQuoteButtons
+          onSubmit={props.onSubmit}
+          onDecline={props.onDecline}
+          isCustomQuote={props.isCustomQuote}
+        />
+      )}
+      {props.variant === 'submitted' && props.quotePrice && (
+        <div className='w-full flex justify-end'>
+          <QuotePriceForList price={props.quotePrice} />
+        </div>
+      )}
+    </div>
+  );
+}
