38538ae1fc1812d1fc3249ee8fdd128ee7bae6f6
heonq <gkagjsrb@naver.com>
2025-03-12 10:35:32 +0900

    feat: MovingInfo 컴포넌트에 헤더 추가 및 클래스이름 상수로 분리

--- src/components/common/movingInfo/organisms/movingInfo/index.tsx
+++ src/components/common/movingInfo/organisms/movingInfo/index.tsx
@@ -1,6 +1,11 @@
 import formatDate from '@/utils/formatDate';
 import MovingInfoRow from '../../molecules/movingInfoRow';
 import { MovingInfoProps } from './movingInfo.types';
+import MovingInfoHeader from '../../atoms/movingInfoHeader';
+import {
+  MOVING_INFO_CONTAINER_STYLE,
+  MOVING_INFO_ROWS_CONTAINER_STYLE,
+} from './constants';
 
 export default function MovingInfo({
   requestedDate,
@@ -13,27 +18,30 @@ export default function MovingInfo({
   const formattedMovingDate = formatDate(movingDate, true, true);
 
   return (
-    <div className='flex flex-col gap-[10px] md:gap-[10px] xl:gap-4 px-5 md:px-8 xl:px-10 py-4 md:py-6 xl:py-8 w-full bg-backgroundVariants-100 border-[1px] border-line-100 rounded-[16px]'>
-      <MovingInfoRow
-        title='견적 요청일'
-        content={formattedRequestedDate}
-      />
-      <MovingInfoRow
-        title='서비스'
-        content={movingType}
-      />
-      <MovingInfoRow
-        title='이용일'
-        content={formattedMovingDate}
-      />
-      <MovingInfoRow
-        title='출발지'
-        content={departure}
-      />
-      <MovingInfoRow
-        title='도착지'
-        content={arrival}
-      />
+    <div className={MOVING_INFO_CONTAINER_STYLE}>
+      <MovingInfoHeader />
+      <div className={MOVING_INFO_ROWS_CONTAINER_STYLE}>
+        <MovingInfoRow
+          title='견적 요청일'
+          content={formattedRequestedDate}
+        />
+        <MovingInfoRow
+          title='서비스'
+          content={movingType}
+        />
+        <MovingInfoRow
+          title='이용일'
+          content={formattedMovingDate}
+        />
+        <MovingInfoRow
+          title='출발지'
+          content={departure}
+        />
+        <MovingInfoRow
+          title='도착지'
+          content={arrival}
+        />
+      </div>
     </div>
   );
 }
