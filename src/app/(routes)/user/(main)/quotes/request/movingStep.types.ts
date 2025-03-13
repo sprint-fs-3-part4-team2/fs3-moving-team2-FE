b030ef59886ac6e3dc43e7823e4046c8c48c3fc1
강대원 <alexnori.com@gmail.com>
2025-03-13 18:18:38 +0900

    feat: movingStep.types, 견적 요청 이사 관련 질문 단계 관련 타입 및 props 인터페이스 추가

---
+++ src/app/(routes)/user/(main)/quotes/request/movingStep.types.ts
@@ -0,0 +1,10 @@
+export type StepType = '이사종류' | '이사예정일' | '이사예정시간' | '이사지역';
+
+export interface MovingStepProps {
+  onNext?: () => void;
+  onEdit?: () => void;
+  onFinish?: () => void;
+  onExitEdit?: () => void;
+  step?: StepType;
+  maxCompletedStep?: number;
+}
