3bf32261bc2da99a9d6f41f1169090eeb54ea3a8
heonq <gkagjsrb@naver.com>
2025-03-13 10:21:21 +0900

    feat: 버튼 컴포넌트 수정

--- src/components/common/customerInfo/molecules/requestedQuoteButtons/index.tsx
+++ src/components/common/customerInfo/molecules/requestedQuoteButtons/index.tsx
@@ -1,5 +1,8 @@
 import { RequestedQuoteButtonsProps } from './requestedQuoteButtons.types';
 import CommonBtn from '@/components/common/commonBtn/commonBtn';
+import editIcon from '@/public/icons/edit.svg';
+import cn from '@/utils/cn';
+import Image from 'next/image';
 
 export default function RequestedQuoteButtons({
   onSubmit,
@@ -9,18 +12,33 @@ export default function RequestedQuoteButtons({
   return (
     <div className='flex flex-col md:flex-row xl:flex-row w-full justify-between gap-[11px]'>
       <CommonBtn
-        widthType={isCustomQuote ? 'half' : 'full'}
+        widthType='dynamic'
         heightType='primary'
         onClick={onSubmit}
+        className={cn(
+          'flex items-center justify-center w-full',
+          isCustomQuote && 'md:w-1/2 xl:w-1/2',
+        )}
       >
         견적 보내기
+        <Image
+          src={editIcon}
+          alt='견적 보내기 아이콘'
+          width={24}
+          height={24}
+        />
       </CommonBtn>
       {isCustomQuote && (
         <CommonBtn
-          widthType='half'
+          widthType='dynamic'
           heightType='primary'
+          className={cn(
+            'flex items-center justify-center w-full',
+            isCustomQuote && 'md:w-1/2 xl:w-1/2',
+          )}
           backgroundColorType='white'
-          textColorType='white'
+          textColorType='blue'
+          borderColorsType='blue'
           onClick={onDecline}
         >
           반려
