96b20e6ad42aee05b8915e34180eca09d7936282
강대원 <alexnori.com@gmail.com>
2025-03-13 09:31:14 +0900

    feat: 모달에 slideUp 애니메이션 효과 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -94,9 +94,14 @@ const config: Config = {
           '0%': { opacity: '0' },
           '100%': { opacity: '1' },
         },
+        slideUp: {
+          '0%': { transform: 'translateY(10%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        }
       },
       animation: {
         fadeIn: 'fadeIn 2.5s ease-in-out forwards',
+        slideUp: 'slideUp 0.5s ease-out forwards',
       },
       boxShadow:{
         'primary' : '2px 2px 10px 0px rgba(220,220,220,0.2), -2px -2px 10px 0px rgba(220,220,220,0.2)',
