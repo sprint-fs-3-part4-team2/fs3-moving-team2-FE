8ccb5d6b487589108fff78ac1a4676f6d5b5636e
heziss <heziss0965@gmail.com>
2025-03-14 13:23:08 +0900

    fix: test 페이지 삭제

--- src/app/(routes)/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import SnsLogin from '@/components/common/SnsLogin';
-
-export default function TestPage() {
-  return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-      <SnsLogin />
-    </div>
-  );
-}
--- src/app/(routes)/user/(main)/movers/[id]/page.tsx
+++ src/app/(routes)/user/(main)/movers/[id]/page.tsx
@@ -1,1 +1,25 @@
-export default function Page() {}
+import MoverStatInfo from '@/components/common/moverInfo/organisms/moverStatInfo';
+
+export default function Page() {
+  // 기사님 정보
+
+  return (
+    <div>
+      <section className='flex items-center justify-center mt-[144px]'>
+        <div>
+          <MoverStatInfo
+            imageUrl='/img/sample-profile/sample-1.svg'
+            moverName='홍길동 이사'
+            rating={4.8}
+            ratingCount={120}
+            experienceYears={10}
+            isFavorite={false}
+            favoriteCount={45}
+            quoteCount={30}
+            isFavoriteMoverList={false}
+          />
+        </div>
+      </section>
+    </div>
+  );
+}
