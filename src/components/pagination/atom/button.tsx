3bf5f9a7ea087b28a350bd48ca02ce3c9c82ee1f
jonghun4 <xwhdgns@gmail.com>
2025-03-14 09:48:46 +0900

    feat: Pagination 컴포넌트 구현

---
+++ src/components/pagination/atom/button.tsx
@@ -0,0 +1,19 @@
+import { MouseEvent, ReactNode } from 'react';
+
+interface Props {
+  className?: string;
+  children?: ReactNode;
+  onClick: (e: MouseEvent) => void;
+}
+
+export default function Button({ children, className = '', onClick }: Props) {
+  const classNames = `w-[34px] h-[34px] text-lg text-black-400 font-semibold ${className} xl:w-[48px] h-[48px] text-2lg`;
+  return (
+    <button
+      className={classNames}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
