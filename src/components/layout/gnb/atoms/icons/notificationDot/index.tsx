c2511eae902547884fcc5604684a563c3f34d94f
Jinhan <wlsgks333@naver.com>
2025-03-17 14:16:16 +0900

    fix: NotificationDot 미디어 쿼리 적용

--- src/components/layout/gnb/atoms/icons/notificationDot/index.tsx
+++ src/components/layout/gnb/atoms/icons/notificationDot/index.tsx
@@ -3,4 +3,4 @@ export default function NotificationDot(): JSX.Element {
 }
 
 const NOTIFICATION_DOT_STYLES =
-  'absolute top-[0px] right-[5px] bg-red-500 rounded-full w-[11px] h-[11px] xl:w-[12px] xl:h-[12px] shadow-primary';
+  'absolute top-[0px] right-[5px] bg-red-500 rounded-full w-[8px] h-[8px] xl:w-[12px] xl:h-[12px] shadow-primary';
