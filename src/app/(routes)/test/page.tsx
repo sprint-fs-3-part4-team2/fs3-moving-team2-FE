b50bf0e9b163194cbbf8eec691bdc2adc5631a07
Jinhan <wlsgks333@naver.com>
2025-03-17 13:20:30 +0900

    fix: 컴포넌트 적용

--- src/app/(routes)/test/page.tsx
+++ src/app/(routes)/test/page.tsx
@@ -1,9 +1,23 @@
+'use client';
+
+import ReviewHeader from '@/components/common/reviewBlock/organisms/reviewHeader';
+import ReviewBlock from '@/components/common/reviewBlock/template/reviewBlock';
+import RatingStars from '@/components/common/shared/molecules/ratingStars';
 import SnsLogin from '@/components/common/SnsLogin';
+import { useState } from 'react';
 
 export default function TestPage() {
+  const [rating, setRating] = useState(0);
+
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
-      <SnsLogin />
+      <ReviewBlock
+        name='안성재'
+        writtenAt='2025-03-14'
+        rating={3}
+        content='정말 멋진 이사였습니다. 이븐한 이사라고 할 수 있겠네요.'
+        className=''
+      />
     </div>
   );
 }
--- src/components/common/reviewBlock/organisms/reviewHeader/index.tsx
+++ src/components/common/reviewBlock/organisms/reviewHeader/index.tsx
@@ -1,7 +1,7 @@
 import cn from '@/utils/cn';
 import NameDateDisplay from '../../molecules/nameDateDisplay';
-import StarRating from '../../molecules/starRating';
 import { ReviewHeaderProp } from './reviewHeader.type';
+import RatingStars from '@/components/common/shared/molecules/ratingStars';
 
 export default function ReviewHeader({
   name,
@@ -9,13 +9,16 @@ export default function ReviewHeader({
   rating,
   className,
 }: ReviewHeaderProp): JSX.Element {
+  
   return (
     <div className={cn('flex flex-col space-y-2', className)}>
       <NameDateDisplay
         name={name}
         writtenAt={writtenAt}
       />
-      <StarRating rating={rating} />
+      <RatingStars
+        rating={rating}
+      />
     </div>
   );
 }
