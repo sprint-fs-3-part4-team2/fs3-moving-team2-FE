2342a372a5e7231292ee077ea3c32064f90aa710
Jinhan <wlsgks333@naver.com>
2025-03-17 10:11:59 +0900

    style: Atom gnbLayout style 지정

---
+++ src/components/layout/gnb/atoms/layout/gnbLayout/constant.ts
@@ -0,0 +1,2 @@
+export const GNB_LAYOUT_STYLES =
+  'flex justify-between items-center w-auto h-[54px] xxl:h-[88px] bg-background border-b-[1px] border-line-100 mx-[24px] md:mx-[72px] xl:mx-[260px]';
