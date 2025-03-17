85c5f26dfcaf3b2e9722f34c459e8818f2e7decf
Jinhan <wlsgks333@naver.com>
2025-03-17 15:40:36 +0900

    feat: gnb 로그인 버튼 추가 및 useRouter 사용

--- src/components/layout/gnb/organisms/gnbRightSection/index.tsx
+++ src/components/layout/gnb/organisms/gnbRightSection/index.tsx
@@ -1,24 +1,50 @@
 'use client';
 
+import CommonButton from '@/components/common/commonBtn/commonBtn';
 import MenuIcon from '../../atoms/icons/menuIcon';
 import Notification from '../../molecules/notification/imdex';
 import Profile from '../../molecules/profileSecrion';
 import { GNB_RIGHT_SECTION_BOX_STYLES } from './constant';
 import { GNBRightSectionProps } from './gnbRightSection.type';
+import { useRouter } from 'next/navigation';
 
 export default function GNBRightSection({
+  isUserAuthorized,
   hasNotification,
   userName,
   imageUrl,
 }: GNBRightSectionProps) {
+  const router = useRouter();
+
+  const handleOnClick = () => {
+    router.push('/select-role');
+  };
+
   return (
-    <div className={GNB_RIGHT_SECTION_BOX_STYLES}>
-      <Notification hasNotification={hasNotification} />
-      <Profile
-        userName={userName}
-        imageUrl={imageUrl ? imageUrl : '/icons/gnb/default-profile.svg'}
-      />
-      <MenuIcon />
+    <div>
+      {isUserAuthorized ? (
+        <div className={GNB_RIGHT_SECTION_BOX_STYLES}>
+          <Notification hasNotification={hasNotification} />
+          <Profile
+            userName={userName}
+            imageUrl={imageUrl ? imageUrl : '/icons/gnb/default-profile.svg'}
+          />
+          <MenuIcon />
+        </div>
+      ) : (
+        <div className='w-[116px]'>
+          <CommonButton
+            onClick={handleOnClick}
+            widthType='full'
+            heightType='tertiary'
+            backgroundColorType='blue'
+            borderColorsType='blue'
+            className='text-2lg font-semibold'
+          >
+            로그인
+          </CommonButton>
+        </div>
+      )}
     </div>
   );
 }
