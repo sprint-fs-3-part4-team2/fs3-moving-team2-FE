379271578ebae950b29791b9315d8906d6047514
Jinhan <wlsgks333@naver.com>
2025-03-17 10:07:59 +0900

    feat: Atom notificationDot 컴포넌트 구현

---
+++ src/components/layout/gnb/atoms/icons/notificationDot/index.tsx
@@ -0,0 +1,6 @@
+export default function NotificationDot(): JSX.Element {
+  return <span className={NOTIFICATION_DOT_STYLES}></span>;
+}
+
+const NOTIFICATION_DOT_STYLES =
+  'absolute top-[-1px] right-[1px] bg-red-500 rounded-full w-[11px] h-[11px] xl:w-[12px] xl:h-[12px] shadow-primary ';
