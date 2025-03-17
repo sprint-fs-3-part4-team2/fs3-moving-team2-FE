420ae0d8751703a55bcc941d5a43151623ea9fac
Jinhan <wlsgks333@naver.com>
2025-03-17 23:42:23 +0900

    chore: GNBRightSectionProps type 추가

--- src/components/layout/gnb/organisms/gnbRightSection/gnbRightSection.type.ts
+++ src/components/layout/gnb/organisms/gnbRightSection/gnbRightSection.type.ts
@@ -1,10 +1,12 @@
 import { MenuIconProp } from '../../atoms/icons/menuIcon/menuIcon.type';
+import { GnbMenuProps } from '../../molecules/gnbMenu/gnbMenu.type';
 import { NotificationProps } from '../../molecules/notification/notification.type';
 import { ProfileSecrionProps } from '../../molecules/profileSecrion/profileSecrion.type';
 
 export interface GNBRightSectionProps
   extends NotificationProps,
     ProfileSecrionProps,
-    MenuIconProp {
+  MenuIconProp,
+  GnbMenuProps{
   isUserAuthorized: boolean;
 }
