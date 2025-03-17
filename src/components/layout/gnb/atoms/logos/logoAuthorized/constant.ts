43a6945e551b166de4927f90496190fc57bcd096
Jinhan <wlsgks333@naver.com>
2025-03-17 10:16:24 +0900

    style: Atom LogoAuthorized style 지정

---
+++ src/components/layout/gnb/atoms/logos/logoAuthorized/constant.ts
@@ -0,0 +1,4 @@
+export const LOGO_WITH_TEXT_STYLES =
+  'relative display-none md:w-[88px] md:h-[34px] xl:w-[116px] xl:h-[44px]';
+
+export const LOGO_ONLY_STYLES = 'relative md:hidden w-[29px] h-[33px]';
