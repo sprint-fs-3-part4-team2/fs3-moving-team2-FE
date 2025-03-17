3168116dc4a0a60affcf1c0a3842c7cea398f510
Jinhan <wlsgks333@naver.com>
2025-03-17 12:00:40 +0900

    chore: Organisms GNBRightSection type 지정

---
+++ src/components/layout/gnb/organisms/gnbRightSection/gnbRightSection.type.ts
@@ -0,0 +1,8 @@
+import { MenuIconProp } from '../../atoms/icons/menuIcon/menuIcon.type';
+import { NotificationProps } from '../../molecules/notification/notification.type';
+import { ProfileSecrionProps } from '../../molecules/profileSecrion/profileSecrion.type';
+
+export interface GNBRightSectionProps
+  extends NotificationProps,
+  ProfileSecrionProps,
+    MenuIconProp {}
