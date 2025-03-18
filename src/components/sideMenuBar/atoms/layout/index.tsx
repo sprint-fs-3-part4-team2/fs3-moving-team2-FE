efc95de2a6f11696aa7f955dd8aeef8be5ff5506
Jinhan <wlsgks333@naver.com>
2025-03-18 14:47:09 +0900

    chore: style 상수화 적용

--- src/components/sideMenuBar/atoms/layout/index.tsx
+++ src/components/sideMenuBar/atoms/layout/index.tsx
@@ -1,9 +1,9 @@
+import { SIDE_MENU_STYLE } from './constant';
+
 export default function SideMenuLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <div className='w-[220px] h-screen bg-background slideDown xl:hidden'>{children}</div>
-  );
+  return <div className={SIDE_MENU_STYLE}>{children}</div>;
 }
