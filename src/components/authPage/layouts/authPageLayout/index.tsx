3ad397527b8d7d5600391b2f12c0e269c0f3afc4
Jinhan <wlsgks333@naver.com>
2025-03-18 15:15:09 +0900

    feat: Layout AuthPageLayout 컴포넌트 구현

---
+++ src/components/authPage/layouts/authPageLayout/index.tsx
@@ -0,0 +1,10 @@
+import { AuthPageLayoutProps } from './authPageLayout.type';
+import { AUTH_PAGE_CONTENT_STYLES, AUTH_PAGE_LAYOUT_STYLES } from './constants';
+
+export default function AuthPageLayout({ children }: AuthPageLayoutProps) {
+  return (
+    <div className={AUTH_PAGE_LAYOUT_STYLES}>
+      <div className={AUTH_PAGE_CONTENT_STYLES}>{children}</div>
+    </div>
+  );
+}
