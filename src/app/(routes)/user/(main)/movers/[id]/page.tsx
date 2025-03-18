6042e2dae79c3c0da377e33feb9e97bc8baedaaf
heziss <heziss0965@gmail.com>
2025-03-18 11:38:23 +0900

    feat: 상태 추가

--- src/app/(routes)/user/(main)/movers/[id]/page.tsx
+++ src/app/(routes)/user/(main)/movers/[id]/page.tsx
@@ -18,9 +18,22 @@ export default function Page() {
   // 기사님 정보
 
   const [isFavorite, setIsFavorite] = useState(false);
+  const [isQuoteRequested, setIsQuoteRequested] = useState<boolean>(false);
+  const [showModal, setShowModal] = useState<boolean>(false);
 
   const toggleFavorite = () => {
     setIsFavorite((prev) => !prev);
+  };
+
+  const handleQuoteRequest = (): void => {
+    if (!isQuoteRequested) {
+      setShowModal(true);
+    }
+  };
+
+  const confirmQuoteRequest = (): void => {
+    setIsQuoteRequested(true);
+    setShowModal(false);
   };
 
   return (
@@ -44,12 +57,12 @@ export default function Page() {
           />
           <HorizontalDivider />
           {/* 모바일 */}
-          <div className='flex-col gap-10 flex md:flex xl:hidden'>
+          <div className='flex-col gap-10 pl-6 flex md:flex xl:hidden'>
             <ShareButtons text='나만 알기엔 아쉬운 기사님인가요?' />
-            <HorizontalDivider />
           </div>
+          <HorizontalDivider />
 
-          <div className='gap-0'>
+          <div className='gap-0 pl-6'>
             <PageHeader>상세설명</PageHeader>
             <p className='text-2lg text-[18px] font-regular'>
               안녕하세요. 이사업계 경력 7년으로 안전한 이사를 도와드리는
@@ -60,7 +73,7 @@ export default function Page() {
           </div>
           <HorizontalDivider />
 
-          <div className='gap-0'>
+          <div className='gap-0 pl-6'>
             <PageHeader>제공 서비스</PageHeader>
             <div className='flex gap-3'>
               <ServiceBadge color='blue'>소형이사</ServiceBadge>
@@ -69,7 +82,7 @@ export default function Page() {
           </div>
           <HorizontalDivider />
 
-          <div className='gap-0'>
+          <div className='gap-0 pl-6'>
             <PageHeader>서비스 가능 지역</PageHeader>
             <div className='flex gap-3'>
               <ServiceBadge color='gray'>서울</ServiceBadge>
@@ -141,46 +154,73 @@ export default function Page() {
         {/* 데스크탑 */}
         <div className='w-[354px] gap-[40px] hidden md:hidden xl:flex flex-col'>
           <p className='text-xl font-semibold'>
-            김코드 기사님에게 지정 견적을 요청해보세요!{' '}
+            김코드 기사님에게 지정 견적을 요청해보세요!
           </p>
-          <CommonButton
-            widthType='full'
-            heightType='primary'
-            backgroundColorType='white'
-            textColorType='black'
-            borderColorsType='gray'
-            className='flex items-center justify-center gap-2'
-          >
-            <Image
-              src={isFavorite ? redFilledHeart : filledHeart}
-              alt='찜하기'
-              width={24}
-              height={24}
-            />
-            기사님 찜하기
-          </CommonButton>
+
+          {!isQuoteRequested && (
+            <CommonButton
+              widthType='full'
+              heightType='primary'
+              backgroundColorType='white'
+              textColorType='black'
+              borderColorsType='gray'
+              className='flex items-center justify-center gap-2'
+              onClick={toggleFavorite}
+            >
+              <Image
+                src={isFavorite ? redFilledHeart : filledHeart}
+                alt='찜하기'
+                width={24}
+                height={24}
+              />
+              기사님 찜하기
+            </CommonButton>
+          )}
+
           <CommonButton
             widthType='full'
             heightType='primary'
             backgroundColorType='blue'
             textColorType='white'
+            onClick={handleQuoteRequest}
+            disabled={isQuoteRequested}
           >
-            지정 견적 요청하기
+            {isQuoteRequested ? '지정 견적 요청 완료' : '지정 견적 요청하기'}
           </CommonButton>
+
           <HorizontalDivider />
           <ShareButtons text='견적서 공유하기' />
         </div>
 
         {/* 모바일 하단 고정 버튼 */}
         <div className='fixed md:fixed xl:hidden bottom-0 left-0 right-0 w-full px-[72px] max-w-[1400px] mx-auto py-[10px] border-t border-t-gray-50 bg-white'>
-          <CommonButton
-            widthType='full'
-            heightType='primary'
-            backgroundColorType='blue'
-            textColorType='white'
-          >
-            지정 견적 요청하기
-          </CommonButton>
+          <div className='flex gap-2'>
+            <CommonButton
+              widthType='dynamic'
+              heightType='primary'
+              backgroundColorType='white'
+              textColorType='black'
+              borderColorsType='gray'
+              className='flex items-center justify-center p-[15px]'
+              onClick={toggleFavorite}
+            >
+              <Image
+                src={isFavorite ? redFilledHeart : filledHeart}
+                alt='찜하기'
+                width={28}
+                height={28}
+              />
+            </CommonButton>
+            <CommonButton
+              widthType='full'
+              heightType='primary'
+              backgroundColorType='blue'
+              textColorType='white'
+              onClick={handleQuoteRequest}
+            >
+              {isQuoteRequested ? '지정 견적 요청 완료' : '지정 견적 요청하기'}
+            </CommonButton>
+          </div>
         </div>
       </div>
     </div>
