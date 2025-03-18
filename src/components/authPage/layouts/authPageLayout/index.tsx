e114163a116bc8416c92594030b0630c11fd9c3a
Jinhan <wlsgks333@naver.com>
2025-03-18 15:49:00 +0900

    chore: JSX.Element 타입 지정

--- src/components/authPage/layouts/authPageLayout/index.tsx
+++ src/components/authPage/layouts/authPageLayout/index.tsx
@@ -1,7 +1,7 @@
 import { AuthPageLayoutProps } from './authPageLayout.type';
 import { AUTH_PAGE_CONTENT_STYLES, AUTH_PAGE_LAYOUT_STYLES } from './constants';
 
-export default function AuthPageLayout({ children }: AuthPageLayoutProps) {
+export default function AuthPageLayout({ children }: AuthPageLayoutProps): JSX.Element {
   return (
     <div className={AUTH_PAGE_LAYOUT_STYLES}>
       <div className={AUTH_PAGE_CONTENT_STYLES}>{children}</div>
--- src/components/authPage/layouts/formGroupLayout/index.tsx
+++ src/components/authPage/layouts/formGroupLayout/index.tsx
@@ -1,6 +1,8 @@
 import { FORM_GROUP_LAYOUT_STYLES } from './constant';
 import { FormGroupLayoutProps } from './formGroupLayout.type';
 
-export default function FormGroupLayout({ children }: FormGroupLayoutProps) {
+export default function FormGroupLayout({
+  children,
+}: FormGroupLayoutProps): JSX.Element {
   return <div className={FORM_GROUP_LAYOUT_STYLES}>{children}</div>;
 }
