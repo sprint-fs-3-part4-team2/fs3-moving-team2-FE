0be1fd12954aa95ce5c71c29d92637a5cc92b04a
Jinhan <wlsgks333@naver.com>
2025-03-18 17:21:02 +0900

    chore: type 재지정

--- src/components/authPage/layouts/authPageLayout/index.tsx
+++ src/components/authPage/layouts/authPageLayout/index.tsx
@@ -1,7 +1,7 @@
-import { AuthPageLayoutProps } from './authPageLayout.type';
+import { ChildrenProp } from '../../common.types';
 import { AUTH_PAGE_CONTENT_STYLES, AUTH_PAGE_LAYOUT_STYLES } from './constants';
 
-export default function AuthPageLayout({ children }: AuthPageLayoutProps): JSX.Element {
+export default function AuthPageLayout({ children }: ChildrenProp): JSX.Element {
   return (
     <div className={AUTH_PAGE_LAYOUT_STYLES}>
       <div className={AUTH_PAGE_CONTENT_STYLES}>{children}</div>
--- src/components/authPage/layouts/formGroupLayout/index.tsx
+++ src/components/authPage/layouts/formGroupLayout/index.tsx
@@ -1,8 +1,8 @@
+import { ChildrenProp } from '../../common.types';
 import { FORM_GROUP_LAYOUT_STYLES } from './constant';
-import { FormGroupLayoutProps } from './formGroupLayout.type';
 
 export default function FormGroupLayout({
   children,
-}: FormGroupLayoutProps): JSX.Element {
+}: ChildrenProp): JSX.Element {
   return <div className={FORM_GROUP_LAYOUT_STYLES}>{children}</div>;
 }
