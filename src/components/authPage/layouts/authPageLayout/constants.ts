56256a09b80dd4631000d5e178a80b4cbd70bdf8
Jinhan <wlsgks333@naver.com>
2025-03-18 15:13:48 +0900

    style: Layout AuthPageLayout style 추가

---
+++ src/components/authPage/layouts/authPageLayout/constants.ts
@@ -0,0 +1,5 @@
+export const AUTH_PAGE_LAYOUT_STYLES =
+  'flex items-start justify-center h-screen pt-[56px]';
+
+export const AUTH_PAGE_CONTENT_STYLES =
+  'flex flex-col items-center justify-start h-screen';
