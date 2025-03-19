2e172fa5fce9ae71fb6aa98dbe9b2a4401a785b1
Jinhan <wlsgks333@naver.com>
2025-03-19 19:34:41 +0900

    chore: Molecule ActionButtonSection 스타일 및 pageTypeContent 객체 작성

---
+++ src/components/authPage/molecules/actionButtonSection/constant.ts
@@ -0,0 +1,15 @@
+export const pageTypeContent = {
+  signIn: {
+    text: '아직 무빙 회원이 아니신가요?',
+    linkText: '이메일로 회원가입하기',
+    buttonText: '로그인',
+  },
+  signUp: {
+    text: '이미 무빙 회원이신가요?',
+    linkText: '로그인',
+    buttonText: '시작하기',
+  },
+};
+
+export const ACTION_BUTTON_SECRION_LAYOUT_STYLES =
+  'w-full flex flex-col justify-center items-center gap-[16px] mb-[48px]';
