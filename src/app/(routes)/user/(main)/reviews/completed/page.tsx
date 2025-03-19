7c387b19c64ca9d41d1dfe41307f2c73b7d4b3ca
Zero5338 <a78559@naver.com>
2025-03-19 14:17:40 +0900

    style:mover-mypage페이지네이션 구현

--- src/app/(routes)/user/(main)/reviews/completed/page.tsx
+++ src/app/(routes)/user/(main)/reviews/completed/page.tsx
@@ -1,54 +1,9 @@
-'use client';
-import { useState } from 'react';
-import MoverInfo from '@/components/common/moverInfo/templates/moverInfo';
-import { MockDataList } from './MockData';
-import Pagination from '@/components/pagination/molecule/pagination';
+import Link from 'next/link';
 
 export default function Page() {
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 6;
-  const totalPages = Math.ceil(MockDataList.length / itemsPerPage);
-
-  const handlePageChange = (page: number) => {
-    setCurrentPage(page);
-  };
-
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-  const currentData = MockDataList.slice(startIndex, endIndex);
-
   return (
-    <div className='flex flex-col items-center mx-auto'>
-      <div className='max-w-[1400px] mx-auto grid grid-cols-1 xl:grid-cols-2 xl:gap-x-[24px] gap-y-[32px] xl:gap-y-[48px] pt-[40px] pb-[24px]'>
-        {currentData.map((data) => (
-          <div
-            key={data.driverName}
-            className='bg-white'
-          >
-            <MoverInfo
-              variant='review'
-              subVariant='written'
-              moverName={data.driverName}
-              movingType={data.movingType}
-              isCustomQuote={data.isCustomQuote}
-              quoteState={data.quoteState}
-              movingDate={data.movingDate}
-              price={data.price}
-              reviewContent={data.reviewContent}
-              rating={data.rating}
-              writtenAt={data.writtenAt}
-            />
-          </div>
-        ))}
-      </div>
-      {totalPages > 1 && (
-        <Pagination
-          currentPage={currentPage}
-          totalPages={totalPages}
-          onPageChange={handlePageChange}
-          className='mx-auto'
-        />
-      )}
+    <div className='px-[24px] md:px-[72px] xl:px-[0px] xl:max-w-[1200px] xl:mx-auto h-[54px] xl:h-[96px] flex gap-[24px] xl:gap-[40px] font-[700] text-[14px]/[54px] xl:text-[24px]/[96px]'>
+      <Link href='/user/reviews/pending'></Link>
     </div>
   );
 }
