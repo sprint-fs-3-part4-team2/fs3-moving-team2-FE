e9869540d83323f79dd5f128fb52ec28c0decf57
Jinhan <wlsgks333@naver.com>
2025-03-19 19:38:03 +0900

    feat: Molecule LogoSection 컴포넌트 구현

---
+++ src/components/authPage/molecules/logoSection/index.tsx
@@ -0,0 +1,28 @@
+import cn from '@/utils/cn';
+import AuthLogo from '../../atoms/authLogo';
+import CustomLinkText from '../../atoms/customLinkText';
+import CustomText from '../../atoms/customText';
+import { userTypeContent } from './constants';
+import { LogoSectionProps } from './logoSection.type';
+
+export default function LogoSection({
+  userType = 'user',
+  moveToPage = 'mover/sign-in',
+}: LogoSectionProps) {
+  const { text, linkText } = userTypeContent[userType];
+
+  return (
+    <div
+      className={cn(
+        'flex flex-col justify-center items-center gap-[20px]',
+        'xl:gap-[30px]',
+      )}
+    >
+      <AuthLogo />
+      <div>
+        <CustomText>{text}</CustomText>
+        <CustomLinkText endpoint={moveToPage}>{linkText}</CustomLinkText>
+      </div>
+    </div>
+  );
+}
