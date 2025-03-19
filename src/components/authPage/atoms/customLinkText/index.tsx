66204cd4c05e8c7886c62c8434ea07acef91604e
Jinhan <wlsgks333@naver.com>
2025-03-19 19:30:32 +0900

    feat: CustomLinText 컴포넌트 구현

--- src/components/authPage/atoms/customLinkText/index.tsx
+++ src/components/authPage/atoms/customLinkText/index.tsx
@@ -1,14 +1,23 @@
-import { Link } from 'lucide-react';
+import Link from 'next/link';
 import cn from '@/utils/cn';
 import { CustomLinkTextProps } from './customLinkText.type';
 
-export default function CustomLinkText({ url, children }: CustomLinkTextProps) {
+export default function CustomLinkText({
+  endpoint,
+  children,
+}: CustomLinkTextProps) {
   return (
     <Link
-      href={url}
-      key={url}
+      href={endpoint}
+      key={endpoint}
     >
-      <span className={cn('text-xs text-blue-300 font-semibold', 'xl:text-xl')}>
+      <span
+        className={cn(
+          'text-xs text-blue-400 font-semibold ml-[4px] underline',
+          'xl:text-xl xl:ml-[8px]',
+          'hover:text-blue-600',
+        )}
+      >
         {children}
       </span>
     </Link>
