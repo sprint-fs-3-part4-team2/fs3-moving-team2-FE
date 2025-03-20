a27e91d530ce55f5a4fcbdb60eec3327c2a16f65
Jinhan <wlsgks333@naver.com>
2025-03-21 00:46:20 +0900

    chore: 불필요 파일 삭제

--- src/components/authPage/molecules/actionButtonSection/ActionButtonSection.type.ts
+++
@@ -1,8 +0,0 @@
-import { PagePath } from '../../common.types';
-
-type PageType = 'signIn' | 'signUp';
-
-export interface ActionButtonSectionProps {
-  pageType: PageType;
-  moveToPage: PagePath;
-}
