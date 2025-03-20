a76dfa14e56561ca391e6b3bb9ca6c0a7cb36ba8
Jinhan <wlsgks333@naver.com>
2025-03-21 00:47:41 +0900

    feat: Logo에 Link 컴포넌트 적용

--- src/components/authPage/atoms/authLogo/index.tsx
+++ src/components/authPage/atoms/authLogo/index.tsx
@@ -1,15 +1,18 @@
 import Image from 'next/image';
 import { AUTH_LOGO_STYLES } from './constant';
+import Link from 'next/link';
 
 export default function AuthLogo(): JSX.Element {
   return (
     <div className={AUTH_LOGO_STYLES}>
-      <Image
-        src='/img/logo/logo-without-icon.svg'
-        alt='Logo only Text'
-        fill
-        className='object-contain'
-      />
+      <Link href='/'>
+        <Image
+          src='/img/logo/logo-without-icon.svg'
+          alt='Logo only Text'
+          fill
+          className='object-contain'
+        />
+      </Link>
     </div>
   );
 }
