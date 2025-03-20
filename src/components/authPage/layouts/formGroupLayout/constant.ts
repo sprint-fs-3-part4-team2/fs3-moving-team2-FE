fbbe83fe20d8c11195835b77c108ac0b004279cb
Jinhan <wlsgks333@naver.com>
2025-03-21 00:51:16 +0900

    fix: FORM_GROUP_LAYOUT_STYLES 스타일 수정

--- src/components/authPage/layouts/formGroupLayout/constant.ts
+++ src/components/authPage/layouts/formGroupLayout/constant.ts
@@ -1,1 +1,2 @@
-export const FORM_GROUP_LAYOUT_STYLES = 'flex flex-col gap-4 xl:gap-8';
+export const FORM_GROUP_LAYOUT_STYLES =
+  'flex flex-col gap-4 w-full mt-[40px] mb-[32px] xl:mb-[56px] xl:gap-8 xl:mt-[72px]';
