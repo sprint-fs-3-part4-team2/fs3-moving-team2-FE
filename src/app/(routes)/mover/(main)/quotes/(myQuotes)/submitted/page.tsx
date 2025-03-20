fdbdf16b9af374c3a9eb1695ce0eeb41d4784ce4
heonq <gkagjsrb@naver.com>
2025-03-20 16:44:17 +0900

    feat: 기사님이 보낸 견적 목록 조회하는 페이지,기능 수정

--- src/app/(routes)/mover/(main)/quotes/(myQuotes)/submitted/page.tsx
+++ src/app/(routes)/mover/(main)/quotes/(myQuotes)/submitted/page.tsx
@@ -1,114 +1,44 @@
-import CustomerInfo from '@/components/common/customerInfo/templates/customerInfo';
+'use client';
 
-const Quotes = [
-  {
-    quoteId: '1',
-    variant: 'submitted',
-    movingType: ['small'],
-    quoteState: 'confirmedQuote',
-    isCustomQuote: true,
-    customerName: '김인서',
-    movingDate: new Date(),
-    arrival: '경기 김포',
-    departure: '서울 중구',
-    quotePrice: 50000,
-    completed: false,
-    declined: false,
-  },
-  {
-    quoteId: '1',
-    variant: 'submitted',
-    movingType: ['small'],
-    quoteState: 'confirmedQuote',
-    isCustomQuote: true,
-    customerName: '김인서',
-    movingDate: new Date(),
-    arrival: '경기 김포',
-    departure: '서울 중구',
-    quotePrice: 50000,
-    completed: false,
-    declined: false,
-  },
-  {
-    quoteId: '1',
-    variant: 'submitted',
-    movingType: ['small'],
-    quoteState: 'confirmedQuote',
-    isCustomQuote: true,
-    customerName: '김인서',
-    movingDate: new Date(),
-    arrival: '경기 김포',
-    departure: '서울 중구',
-    quotePrice: 50000,
-    completed: true,
-    declined: false,
-  },
-  {
-    quoteId: '1',
-    variant: 'submitted',
-    movingType: ['small'],
-    quoteState: 'confirmedQuote',
-    isCustomQuote: true,
-    customerName: '김인서',
-    movingDate: new Date(),
-    arrival: '경기 김포',
-    departure: '서울 중구',
-    quotePrice: 50000,
-    completed: true,
-    declined: false,
-  },
-  {
-    quoteId: '1',
-    variant: 'submitted',
-    movingType: ['small'],
-    quoteState: 'confirmedQuote',
-    isCustomQuote: true,
-    customerName: '김인서',
-    movingDate: new Date(),
-    arrival: '경기 김포',
-    departure: '서울 중구',
-    quotePrice: 50000,
-    completed: true,
-    declined: false,
-  },
-  {
-    quoteId: '1',
-    variant: 'submitted',
-    movingType: ['small'],
-    quoteState: 'confirmedQuote',
-    isCustomQuote: true,
-    customerName: '김인서',
-    movingDate: new Date(),
-    arrival: '경기 김포',
-    departure: '서울 중구',
-    quotePrice: 50000,
-    completed: true,
-    declined: false,
-  },
-];
+import CustomerInfo from '@/components/common/customerInfo/templates/customerInfo';
+import { getSubmittedQuotesList } from '@/services/quotes';
+import { useQuery } from '@tanstack/react-query';
+import { useSearchParams } from 'next/navigation';
 
 export default function Page() {
+  const searchParams = useSearchParams();
+  const page = Number(searchParams.get('page') || 1);
+  const pageSize = Number(searchParams.get('pageSize') || 4);
+  const moverId = 'cm8cvdyaf006lt47dywv5gugh';
+
+  const { data } = useQuery({
+    queryKey: ['quotes', page, pageSize],
+    queryFn: async () => getSubmittedQuotesList({ page, pageSize, moverId }),
+  });
+
   return (
-    <div className='w-full bg-backgroundVariants-100 h-full py-10'>
-      <div className='grid grid-cols-1 md:grid-cols-1 xl:grid-cols-2 max-w-[1400px] px-6 md:px-[72px] xl:px-0 mx-auto gap-[24px] gap-y-12'>
-        {Quotes.map((quote) => (
-          <CustomerInfo
-            key={quote.customerName}
-            quoteId={quote.quoteId}
-            variant='submitted'
-            movingType={['small']}
-            quoteState='confirmedQuote'
-            isCustomQuote={true}
-            customerName='김인서'
-            movingDate={new Date()}
-            arrival='경기 김포'
-            departure='서울 중구'
-            completed={quote.completed}
-            declined={false}
-            quotePrice={quote.quotePrice}
-          />
-        ))}
+    data && (
+      <div className='w-full bg-backgroundVariants-100 h-full py-10'>
+        <div className='grid grid-cols-1 md:grid-cols-1 xl:grid-cols-2 max-w-[1400px] px-6 md:px-[72px] xl:px-0 mx-auto gap-[24px] gap-y-12'>
+          {data.list.map((quote) => (
+            <CustomerInfo
+              key={quote.customerName}
+              quoteId={quote.id}
+              variant='submitted'
+              movingType={[quote.request.moveType]}
+              quoteState={quote.matched ? 'confirmedQuote' : 'pendingQuote'}
+              isCustomQuote={quote.isCustomRequest}
+              customerName={quote.customerName}
+              movingDate={quote.request.moveDate}
+              arrival={quote.request.arrival.sido}
+              departure={quote.request.departure.sido}
+              completed={quote.completed}
+              declined={false}
+              quotePrice={quote.price}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    )
   );
 }
