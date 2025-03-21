388a259ec145d59ef959594402365d954a426701
JeonJun202 <jjg010202@gmail.com>
2025-03-21 16:25:21 +0900

    feat:알림 서비스 api 추가

--- src/services/notifications.ts
+++ src/services/notifications.ts
@@ -1,3 +1,12 @@
-// alarm 목록 가져오기
+import axiosInstance from '@/lib/axiosInstance';
 
+// alarm 목록 가져오기
+export const getNotifications = async () => {
+  const notifications = await axiosInstance.get('/notification');
+  return notifications.data;
+};
 // alarm 읽기 처리
+export const updateAlarm = async (alarmId: string) => {
+  const updatedAlarm = await axiosInstance.patch(`/notification/:${alarmId}`);
+  return updatedAlarm.data;
+};
