9ba7596ef5cb5637d077b499a47e96aa4b85ae66
kimhyunmook_mac <gusanr4200@naver.com>
2025-03-21 15:23:08 +0900

    0321 피드백 부분 수정

--- src/components/dropdown/children/profile.tsx
+++ src/components/dropdown/children/profile.tsx
@@ -1,7 +1,7 @@
 'use client';
 import cn from '@/utils/cn';
 import Dropdown, { DropdownProps } from '../dropdown';
-import { PropsWithChildren, useEffect, useState } from 'react';
+import { PropsWithChildren, useEffect, useRef, useState } from 'react';
 import Link, { LinkProps } from 'next/link';
 import { usePathname } from 'next/navigation';
 
@@ -13,20 +13,40 @@ function Profile({
   const [open, setOpen] = useState(isOpen || false);
   const [name, setName] = useState('테스트');
   const pathname = usePathname();
+  const divRef = useRef<HTMLDivElement | null>(null);
   useEffect(() => {
     // 지울 코드
     // 주스탄드로 전역으로 유저정보 처리 할 것
     setName('테스트');
   }, []);
+
   useEffect(() => {
     setOpen(false);
   }, [pathname]);
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (divRef.current && !divRef.current.contains(e.target as Node)) {
+        setOpen(false);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
   function openHandle(e: React.MouseEvent<HTMLDivElement>) {
     e.preventDefault();
     setOpen((prev) => !prev);
   }
+
   return (
-    <div className={cn('relative', className && className)}>
+    <div
+      className={cn('relative', className && className)}
+      ref={divRef}
+    >
       <div
         className='w-full h-full cursor-pointer'
         onClick={openHandle}
@@ -45,9 +65,9 @@ function Profile({
           {name} 고객님
         </h2>
         <ul>
-          <ProfileList href={'#'}>프로필 수정</ProfileList>
-          <ProfileList href={'#'}>찜한 기사님</ProfileList>
-          <ProfileList href={'#'}>이사 리뷰</ProfileList>
+          <ProfileList href={'/user/profile/edit'}>프로필 수정</ProfileList>
+          <ProfileList href={'/user/movers/favorite'}>찜한 기사님</ProfileList>
+          <ProfileList href={'/user/reviews/completed'}>이사 리뷰</ProfileList>
           <li
             className={cn(
               'h-[38px] flex items-center justify-center border-t border-grayscale-50 mt-2',
@@ -81,13 +101,16 @@ function ProfileList({ href, children }: ProfileListProps) {
   return (
     <li
       className={cn(
-        'h-10 py-2 pl-3',
+        'h-10 py-2 pl-3 cursor-pointer',
         'xl:h-[54px] xl:py-[14px] xl:pl-6',
         'hover:bg-grayscale-100',
       )}
     >
       <Link
-        className={cn('text-black-400 text-md font-medium', 'xl:text-lg')}
+        className={cn(
+          'text-black-400 text-md font-medium w-full h-full block',
+          'xl:text-lg',
+        )}
         href={href}
       >
         {children}
