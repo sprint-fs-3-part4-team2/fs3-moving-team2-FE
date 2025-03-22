39a9aa396264b9a706b1a7fa2c648a4fda06bdfd
Jinhan <wlsgks333@naver.com>
2025-03-22 15:09:01 +0900

    refactor: common url 상수화

--- src/components/layout/gnb/atoms/menus/userMenu/index.tsx
+++ src/components/layout/gnb/atoms/menus/userMenu/index.tsx
@@ -1,39 +1,25 @@
 import CustomLink from '../common/customLink';
 import Text from '../common/gnbText';
+import { COMMON_URL } from './constant';
 
 export default function UserMenu(): JSX.Element {
   return (
     <>
       <CustomLink href='/user/quotes/request'>
-        <Text
-          linkHref={[
-            '/user/quotes/request',
-            '/user/reviews/pending',
-            '/user/reviews/completed',
-          ]}
-        >
+        <Text linkHref={['/user/quotes/request', ...COMMON_URL]}>
           견적 요청
         </Text>
       </CustomLink>
       <CustomLink href='/user/movers'>
-        <Text
-          linkHref={[
-            '/user/movers',
-            '/user/reviews/pending',
-            '/user/reviews/completed',
-          ]}
-        >
-          기사님 찾기
-        </Text>
+        <Text linkHref={['/user/movers', ...COMMON_URL]}>기사님 찾기</Text>
       </CustomLink>
       <CustomLink href='/user/quotes/pending'>
         <Text
           linkHref={[
             '/user/quotes/pending',
-            '/user/reviews/pending',
-            '/user/reviews/completed',
             '/user/quotes/requested',
             '/user/quotes/completed',
+            ...COMMON_URL,
           ]}
         >
           내 견적 관리
