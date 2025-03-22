24d7205170b78682347d1754c02612c95bea4a9a
Jinhan <wlsgks333@naver.com>
2025-03-22 14:32:02 +0900

    chore: highlightText 함수 적용

--- src/components/layout/gnb/atoms/menus/common/gnbText/index.tsx
+++ src/components/layout/gnb/atoms/menus/common/gnbText/index.tsx
@@ -3,17 +3,16 @@
 import cn from '@/utils/cn';
 import { COMMON_TEXT_STYLES } from './constant';
 import { GNBTextProps } from './gnbText.type';
-import { usePathname } from 'next/navigation';
+import { highlightText } from '@/utils/highlightText';
 
 export default function Text({ linkHref, children }: GNBTextProps) {
-  const pathname = usePathname();
-  const isActive = pathname === linkHref;
+  const highlighted = highlightText(linkHref as string[]);
 
   return (
     <span
       className={cn(
         COMMON_TEXT_STYLES,
-        isActive ? 'font-black-400' : 'text-grayscale-400',
+        highlighted ? 'font-black-400' : 'text-grayscale-400',
       )}
     >
       {children}
