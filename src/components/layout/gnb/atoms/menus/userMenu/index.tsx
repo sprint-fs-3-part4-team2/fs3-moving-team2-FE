2f62e0a0d5fc94a003b13913d3ecf5913c531c0c
Jinhan <wlsgks333@naver.com>
2025-03-22 14:33:18 +0900

    chore: 컴포넌트 prop으로 linkHref 추가

--- src/components/layout/gnb/atoms/menus/userMenu/index.tsx
+++ src/components/layout/gnb/atoms/menus/userMenu/index.tsx
@@ -5,13 +5,39 @@ export default function UserMenu(): JSX.Element {
   return (
     <>
       <CustomLink href='/user/quotes/request'>
-        <Text linkHref='/user/quotes/request'>견적 요청</Text>
+        <Text
+          linkHref={[
+            '/user/quotes/request',
+            '/user/reviews/pending',
+            '/user/reviews/completed',
+          ]}
+        >
+          견적 요청
+        </Text>
       </CustomLink>
       <CustomLink href='/user/movers'>
-        <Text linkHref='/user/movers'>기사님 찾기</Text>
+        <Text
+          linkHref={[
+            '/user/movers',
+            '/user/reviews/pending',
+            '/user/reviews/completed',
+          ]}
+        >
+          기사님 찾기
+        </Text>
       </CustomLink>
       <CustomLink href='/user/quotes/pending'>
-        <Text linkHref='/user/quotes/pending'>내 견적 관리</Text>
+        <Text
+          linkHref={[
+            '/user/quotes/pending',
+            '/user/reviews/pending',
+            '/user/reviews/completed',
+            '/user/quotes/requested',
+            '/user/quotes/completed',
+          ]}
+        >
+          내 견적 관리
+        </Text>
       </CustomLink>
     </>
   );
