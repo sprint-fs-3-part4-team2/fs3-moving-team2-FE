62da212a93e68598727bac912226e151d02b2461
heonq <gkagjsrb@naver.com>
2025-03-23 19:02:33 +0900

    feat: 견적요청 취소 모달 구현

--- src/components/requestedQuote/templates/quoteRequestExist/index.tsx
+++ src/components/requestedQuote/templates/quoteRequestExist/index.tsx
@@ -3,12 +3,29 @@
 import CommonButton from '@/components/common/commonBtn/commonBtn';
 import MovingInfo from '@/components/common/movingInfo/organisms/movingInfo';
 import { MovingInfoProps } from '@/components/common/movingInfo/organisms/movingInfo/movingInfo.types';
+import ModalWrapper from '@/components/modal/ModalWrapper';
+import CancelRequestModalContent from '../../molecules/cancelRequestModalContent';
+import { useHandleModalOpen } from '@/hooks/useHandleModalOpen';
 
 export default function QuoteRequestExist(
   props: MovingInfoProps & { id: string },
 ) {
+  const { modalOpen, openModal, closeModal } = useHandleModalOpen();
+
   return (
     <div className='relative flex flex-col mx-auto w-full items-center overflow-auto pb-24'>
+      {modalOpen && (
+        <ModalWrapper
+          title='견적 요청 취소하기'
+          onClose={closeModal}
+          className='xl:w-[608px]'
+        >
+          <CancelRequestModalContent
+            onClose={closeModal}
+            requestId='abc'
+          />
+        </ModalWrapper>
+      )}
       <div className='w-full max-w-[1400px] px-6 md:px-[72.5px] xl:px-0 flex justify-between gap-[127px] mt-8 md:mt-8 xl:mt-10'>
         <div className='w-full'>
           <MovingInfo {...props} />
@@ -17,6 +34,7 @@ export default function QuoteRequestExist(
           <CommonButton
             widthType='full'
             heightType='primary'
+            onClick={openModal}
           >
             견적 취소하기
           </CommonButton>
@@ -26,6 +44,7 @@ export default function QuoteRequestExist(
         <CommonButton
           widthType='full'
           heightType='primary'
+          onClick={openModal}
         >
           견적 취소하기
         </CommonButton>
