fcf6981f68b1d0d337760cd63626285c54f4e45c
heonq <gkagjsrb@naver.com>
2025-03-24 17:23:40 +0900

    fix: useHandleModalOpen type 오타 수정

--- src/hooks/useHandleModalOpen.ts
+++ src/hooks/useHandleModalOpen.ts
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 export const useHandleModalOpen = () => {
-  const [modalOpen, setModalOpen] = useState<Boolean>(false);
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
   const openModal = () => setModalOpen(true);
   const closeModal = () => setModalOpen(false);
 
