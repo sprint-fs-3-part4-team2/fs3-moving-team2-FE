28c580feedfe423d6ff3e1012a12ef08d5084500
heonq <gkagjsrb@naver.com>
2025-03-24 14:36:54 +0900

    style: InputSection 컴포넌트 props로 전달받은 클래스네임 입력 위치 수정

--- src/components/common/inputSection/atoms/customInput/inputs/searchInput.tsx
+++ src/components/common/inputSection/atoms/customInput/inputs/searchInput.tsx
@@ -7,12 +7,14 @@ export default function SearchInput({
   placeholder,
   onSearch,
   styleVariant: inputStyle,
+  inputClassName,
 }: SearchTypeProps) {
   const inputProps = {
     className: cn(
       INPUT_STYLES.common,
       INPUT_STYLES[inputStyle],
       'pl-[68px] xl:text-2lg',
+      inputClassName,
     ),
     placeholder,
   };
