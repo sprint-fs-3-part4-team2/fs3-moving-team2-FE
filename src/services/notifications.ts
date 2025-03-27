e1600583612da08f26095dc4866d2e2f14a5efbf
JeonJun202 <jjg010202@gmail.com>
2025-03-27 15:25:57 +0900

    main pull 커밋

--- src/services/notifications.ts
+++ src/services/notifications.ts
@@ -5,8 +5,15 @@ export const getNotifications = async () => {
   const notifications = await axiosInstance.get('/notification');
   return notifications.data;
 };
+
 // alarm 읽기 처리
 export const updateAlarm = async (alarmId: string) => {
   const updatedAlarm = await axiosInstance.patch(`/notification/:${alarmId}`);
   return updatedAlarm.data;
 };
+
+// // alarm 생성 userId 는 알림 받는 user
+// export const createAlarm = async (userId: string) => {
+//   const createdAlarm = await axiosInstance.post(`/notification/:${userId}`);
+//   return createdAlarm.data;
+// };
