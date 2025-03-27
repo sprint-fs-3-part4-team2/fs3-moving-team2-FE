a069c778aec8bc56233bc8208e4db381f3b5ff29
강대원 <alexnori.com@gmail.com>
2025-03-27 09:15:01 +0900

    feat: 받은견적요청 목록 API 관련 인터페이스 추가

--- src/services/quoteRequests.ts
+++ src/services/quoteRequests.ts
@@ -1,6 +1,8 @@
 import axiosInstance from '@/lib/axiosInstance';
 import { QuoteRequest } from './types/quotesDetail/common.types';
 import { QuoteRequestResponse } from './types/quotesDetail/quoteRequests.types';
+import { QuoteRequestsResponse } from './types/allQuotes/allQuoteRequests.types';
+import { is } from 'date-fns/locale';
 
 // 견적 요청 생성
 export const createQuoteRequest = async ({
@@ -39,6 +41,39 @@ export const cancelQuoteRequest = async (requestId: string) => {
     await axiosInstance.delete('/quote-request');
   } catch (error: any) {
     console.error('견적 요청 취소 실패', error);
+    throw error;
+  }
+};
+
+export const getAllQuoteRequests = async (
+  page?: number,
+  pageSize?: number,
+  search?: string,
+  moveType?: string,
+  sortBy?: string,
+  isServiceRegionMatch?: boolean,
+  isTargetedQuote?: boolean,
+) => {
+  try {
+    const { data } = await axiosInstance.get<QuoteRequestsResponse>(
+      '/quote-requests',
+      {
+        params: {
+          page: page || 1,
+          pageSize: pageSize || 10,
+          search: search ? search : undefined,
+          moveType: moveType ? moveType : undefined,
+          sortBy: sortBy ? sortBy : undefined,
+          isServiceRegionMatch: isServiceRegionMatch
+            ? isServiceRegionMatch
+            : undefined,
+          isTargetedQuote: isTargetedQuote ? isTargetedQuote : undefined,
+        },
+      },
+    );
+    return data;
+  } catch (error: any) {
+    console.error('모든 견적 요청 조회 실패', error);
     throw error;
   }
 };
