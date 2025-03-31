b4e5a33c972ebfe2633d31182ffcf5fd7ccc1dfb
Jinhan <wlsgks333@naver.com>
2025-03-31 16:21:24 +0900

    chore: notification

--- src/components/layout/gnb/molecules/notification/imdex.tsx
+++ src/components/layout/gnb/molecules/notification/imdex.tsx
@@ -1,59 +1,66 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import BellIcon from '../../atoms/icons/bellIcon';
 import NotificationDot from '../../atoms/icons/notificationDot';
-import Alarm from '@/components/dropdown/children/alarm';
+import Alarm, { AlarmData } from '@/components/dropdown/children/alarm';
 import { NOTIFICATION_STYLES } from './constant';
+import { getNotificationApi } from '@/services/notification';
+import { readNotificationApi } from '@/services/notification';
+
+async function readAlarm(id: string) {
+  await readNotificationApi(id);
+}
 
 export default function Notification(): JSX.Element {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [data, setData] = useState<AlarmData[]>([]);
 
-  const hasNotification = true;
-  // 알림 목록 조회
+  useEffect(() => {
+    getNotificationApi().then((res) => {
+      setData(res.data ?? []);
+    });
+  }, []);
+
+  useEffect(() => {
+    const eventSource = new EventSource(
+      `${process.env.NEXT_PUBLIC_API_URL || `http://localhost:8080`}/notification/events`,
+    ); // 백엔드 SSE 엔드포인트
+
+    // 메시지 수신
+    eventSource.onmessage = (event) => {
+      setData((prev) => {
+        const parse = JSON.parse(event.data);
+        if (prev.find((x) => x.id === parse.id)) return [...prev];
+        return [parse, ...prev];
+      });
+    };
+
+    // 오류 처리
+    eventSource.onerror = (error) => {
+      console.error('SSE 오류 발생:', error);
+      eventSource.close();
+    };
+
+    return () => {
+      eventSource.close();
+    };
+  }, []);
 
   return (
     <Alarm
-      {...args}
-      isOpen={isOpen}
+      readFn={(id) => {
+        readAlarm(id);
+        setData((prev) => {
+          return prev.map((noti) => {
+            if (noti.id === id) noti.isRead = true;
+            return noti;
+          });
+        });
+      }}
+      data={data}
     >
       <div className={NOTIFICATION_STYLES}>
         <BellIcon />
-        {hasNotification && <NotificationDot />}
+        {data?.filter((x) => !x.isRead).length > 0 && <NotificationDot />}
       </div>
     </Alarm>
   );
 }
-
-const args = {
-  className: '',
-  isOpen: false,
-  data: [
-    {
-      id: 'asdf',
-      is_read: false,
-      create_at: new Date('2025-03-07T12:00:00Z'),
-      message: '메시지',
-      url: '#',
-    },
-    {
-      id: 'asdf2',
-      is_read: false,
-      create_at: new Date('2025-03-06T12:00:00Z'),
-      message: '메시지2',
-      url: '#',
-    },
-    {
-      id: 'asdf3',
-      is_read: false,
-      create_at: new Date('2025-03-10T12:00:00Z'),
-      message: '메시지3',
-      url: '#',
-    },
-    {
-      id: 'asdf4',
-      is_read: false,
-      create_at: new Date('2025-03-10T12:00:00Z'),
-      message: '메시지4',
-      url: '#',
-    },
-  ],
-};
