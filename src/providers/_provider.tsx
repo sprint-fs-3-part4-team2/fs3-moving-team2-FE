e55813c9e566e2fca735dacf4b4d3c4460f8d331
Jinhan <wlsgks333@naver.com>
2025-03-31 14:17:08 +0900

    chore: AuthProvider 삭제

--- src/providers/_provider.tsx
+++ src/providers/_provider.tsx
@@ -1,15 +1,12 @@
 import { PropsWithChildren } from 'react';
 import QueryProvider from '@/providers/queryProvider';
 import ToasterProvider from '@/providers/toastProvider';
-import AuthProvider from './authProvider';
 
 // provider 관련 코드 합치기
 export default function Provider({ children }: PropsWithChildren) {
   return (
     <QueryProvider>
-      <AuthProvider>
-        <ToasterProvider>{children}</ToasterProvider>
-      </AuthProvider>
+      <ToasterProvider>{children}</ToasterProvider>
     </QueryProvider>
   );
 }
