9b1470556c01e206db5fc76a613d0db4cb7fc4e8
Jinhan <wlsgks333@naver.com>
2025-03-31 14:15:27 +0900

    chore: priority prop 추가

--- src/components/authPage/atoms/authLogo/index.tsx
+++ src/components/authPage/atoms/authLogo/index.tsx
@@ -4,15 +4,16 @@ import Link from 'next/link';
 
 export default function AuthLogo(): JSX.Element {
   return (
-    <div className={AUTH_LOGO_STYLES}>
-      <Link href='/'>
+    <Link href='/'>
+      <div className={AUTH_LOGO_STYLES}>
         <Image
           src='/img/logo/logo-without-icon.svg'
           alt='Logo only Text'
           fill
           className='object-contain'
+          priority
         />
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 }
