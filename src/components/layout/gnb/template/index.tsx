249cbe41730d242cdcac7bd8539fc27e79ab2b7b
Jinhan <wlsgks333@naver.com>
2025-03-31 11:32:44 +0900

    chore: commit

--- src/components/layout/gnb/template/index.tsx
+++ src/components/layout/gnb/template/index.tsx
@@ -4,48 +4,6 @@ import { usePathname } from 'next/navigation';
 import GNBLayout from '../atoms/layout/gnbLayout';
 import GNBLogo from '../molecules/gnbLogo';
 import GNBMenu from '../molecules/gnbMenu';
-import GNBRightSection from '../organisms/gnbRightSection';
-import { GNB_LOGO_MENU_STYLES, GNB_STYLES } from './constant';
-import { useUserStore } from '@/store/userStore';
-
-export default function GNB() {
-  const pathname = usePathname();
-  const { user, isAuthorized } = useUserStore();
-
-  const userName = user?.name || '';
-  const imageUrl = user?.imageUrl || '/icons/gnb/default-profile.svg';
-  const userType = user?.role || 'guest';
-
-  console.log('user333: ', isAuthorized);
-  console.log('username333: ', user, user?.name);
-
-  if (pathname === '/select-role') {
-    return null;
-  }
-
-  return (
-    <div className={GNB_STYLES}>
-      <GNBLayout>
-        <div className={GNB_LOGO_MENU_STYLES}>
-          <GNBLogo isAuthorized={isAuthorized} />
-          <GNBMenu userType={userType} />
-        </div>
-        <GNBRightSection
-          isAuthorized={isAuthorized}
-          userName={userName}
-          imageUrl={imageUrl}
-          userType={userType}
-        />
-      </GNBLayout>
-    </div>
-  );
-}
-('use client');
-
-import { usePathname } from 'next/navigation';
-import GNBLayout from '../atoms/layout/gnbLayout';
-import GNBLogo from '../molecules/gnbLogo';
-import GNBMenu from '../molecules/gnbMenu';
 import { GNB_LOGO_MENU_STYLES, GNB_STYLES } from './constant';
 import { useUserStore } from '@/store/userStore';
 import dynamic from 'next/dynamic';
@@ -53,13 +11,7 @@ const GNBRightSection = dynamic(() => import('../organisms/gnbRightSection'), {
   ssr: false,
 });
 
-export default function GNB({
-  isAuthorized,
-  userType,
-  userName,
-  imageUrl,
-  hasNotification,
-}: GNBProps) {
+export default function GNB() {
   const pathname = usePathname();
   const { user, isAuthorized } = useUserStore();
 
