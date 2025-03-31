dece015b8349a6692844ff88a33ffd844c2084e1
jonghun4 <xwhdgns@gmail.com>
2025-03-31 14:19:12 +0900

    test:코드 test

--- src/app/(routes)/user/(main)/quotes/(myQuotes)/completed/page.tsx
+++ src/app/(routes)/user/(main)/quotes/(myQuotes)/completed/page.tsx
@@ -1,1 +1,148 @@
-export default function Page() {}
+'use client';
+
+import { useState, useEffect } from 'react';
+import MoverInfo from '@/components/common/moverInfo/templates/moverInfo';
+import MovingInfo from '@/components/common/movingInfo/organisms/movingInfo';
+
+interface SharedInfoProps {
+  requestedDate: Date;
+  movingDate: Date;
+  departure: string;
+  arrival: string;
+  border: boolean;
+  isOpen: boolean;
+  data: { name: string }[];
+  name: string;
+  dispatch: () => void;
+  moverName: string;
+  isCustomQuote: boolean;
+  quoteState: 'confirmedQuote' | 'pendingQuote';
+  rating: number;
+  experienceYears: number;
+  quoteCount: number;
+  isFavorite: boolean;
+  favoriteCount: number;
+  ratingCount: number;
+  price: number;
+  description: string;
+}
+
+interface MovingInfoProps extends SharedInfoProps {
+  movingType: '소형이사' | '사무실이사' | '가정이사';
+}
+
+interface MoverInfoProps extends SharedInfoProps {
+  movingType: ('small' | 'office' | 'home')[];
+  isFavoriteMoverList: boolean;
+}
+
+export default function Page() {
+  const [randomData, setRandomData] = useState<SharedInfoProps>({
+    requestedDate: new Date(),
+    movingDate: new Date(),
+    departure: '',
+    arrival: '',
+    border: false,
+    isOpen: false,
+    data: [],
+    name: '',
+    dispatch: () => {},
+    moverName: '',
+    isCustomQuote: false,
+    quoteState: 'pendingQuote',
+    rating: 0,
+    experienceYears: 0,
+    quoteCount: 0,
+    isFavorite: false,
+    favoriteCount: 0,
+    ratingCount: 0,
+    price: 0,
+    description: '',
+  });
+
+  const [movingType, setMovingType] = useState<'small' | 'office' | 'home'>(
+    'small',
+  );
+  const movingTypeMapping: Record<
+    'small' | 'office' | 'home',
+    '소형이사' | '사무실이사' | '가정이사'
+  > = {
+    small: '소형이사',
+    office: '사무실이사',
+    home: '가정이사',
+  };
+
+  useEffect(() => {
+    setRandomData({
+      requestedDate: new Date(),
+      movingDate: new Date(),
+      departure:
+        '서울 ' + ['강남구', '서초구', '마포구'][Math.floor(Math.random() * 3)],
+      arrival:
+        '경기도 ' +
+        ['성남시', '수원시', '용인시'][Math.floor(Math.random() * 3)],
+      border: false,
+      isOpen: Math.random() > 0.5,
+      data: [{ name: '확정한 견적서' }],
+      name: '랜덤 드롭다운',
+      dispatch: () => console.log('드롭다운 선택 변경'),
+      moverName: '김코드 ' + Math.floor(Math.random() * 100),
+      isCustomQuote: Math.random() > 0.5,
+      quoteState: Math.random() > 0.5 ? 'confirmedQuote' : 'pendingQuote',
+      rating: Math.floor(Math.random() * 5) + 1,
+      experienceYears: Math.floor(Math.random() * 10) + 1,
+      quoteCount: Math.floor(Math.random() * 500) + 1,
+      isFavorite: Math.random() > 0.5,
+      favoriteCount: Math.floor(Math.random() * 1000) + 1,
+      ratingCount: Math.floor(Math.random() * 1000) + 1,
+      price: Math.floor(Math.random() * 100000) + 30000,
+      description: '최선을 다해 모시겠습니다!',
+    });
+
+    const randomMovingType: 'small' | 'office' | 'home' = [
+      'small',
+      'office',
+      'home',
+    ][Math.floor(Math.random() * 3)] as 'small' | 'office' | 'home';
+    setMovingType(randomMovingType);
+  }, []);
+
+  return (
+    <div className='max-w-[1400px] h-[auto] mx-auto rounded-[40px]'>
+      <div className='mt-4 xl:mt-12'>
+        <MovingInfo
+          requestedDate={randomData.requestedDate}
+          movingDate={randomData.movingDate}
+          movingType={movingTypeMapping[movingType]}
+          departure={randomData.departure}
+          arrival={randomData.arrival}
+        />
+      </div>
+      <div>
+        <p className='text-lg md:mt-12 md:mb-10 xl:text-2xl text-black-400 font-semibold mt-8 mb-6'>
+          견적서 목록
+        </p>
+        <div className='mt-8'>
+          <MoverInfo
+            imageUrl={null}
+            variant='quote'
+            subVariant='completed'
+            moverName={randomData.moverName}
+            movingType={[movingType]}
+            isCustomQuote={randomData.isCustomQuote}
+            quoteState={randomData.quoteState}
+            rating={randomData.rating}
+            experienceYears={randomData.experienceYears}
+            quoteCount={randomData.quoteCount}
+            isFavorite={randomData.isFavorite}
+            favoriteCount={randomData.favoriteCount}
+            ratingCount={randomData.ratingCount}
+            price={randomData.price}
+            isFavoriteMoverList={false}
+            description={randomData.description}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/(routes)/user/(main)/quotes/(myQuotes)/pending/page.tsx
+++ src/app/(routes)/user/(main)/quotes/(myQuotes)/pending/page.tsx
@@ -1,1 +1,269 @@
-export default function Page() {}
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import MoverInfo from '@/components/common/moverInfo/templates/moverInfo';
+import Pagination from '@/components/pagination/molecule/pagination';
+import axiosInstance from '@/lib/axiosInstance';
+
+interface Address {
+  id: string;
+  quoteRequestId: string;
+  sido: string;
+  sigungu: string;
+  street: string;
+  fullAddress: string;
+  type: 'DEPARTURE' | 'ARRIVAL';
+}
+
+interface Mover {
+  id: string;
+  userId: string;
+  profileImage: string;
+  experienceYears: number;
+  introduction: string;
+  description: string;
+  averageRating: number;
+  totalReviews: number;
+  totalCustomerFavorite: number;
+  totalConfirmedCount: number;
+  createdAt: string;
+  updatedAt: string;
+  moverServices: MoverService[];
+}
+
+interface MoverService {
+  id: string;
+  moverId: string;
+  serviceType: 'SMALL_MOVE' | 'OFFICE_MOVE' | 'HOME_MOVE';
+  createdAt: string;
+}
+
+interface MoverQuote {
+  id: string;
+  moverId: string;
+  quoteRequestId: string;
+  price: number;
+  comment: string;
+  createdAt: string;
+  updatedAt: string;
+  mover: Mover;
+}
+
+interface Quote {
+  moveDate: string;
+  quoteRequestAddresses: Address[];
+  moverQuotes: MoverQuote[];
+}
+
+type QuoteState = 'confirmedQuote' | 'pendingQuote';
+type Variant = 'quote';
+type SubVariant = 'pending';
+
+interface MoverInfoTemplateProps {
+  variant: Variant;
+  subVariant: SubVariant;
+  moverName: string;
+  movingType: ('small' | 'office' | 'home')[];
+  isCustomQuote: boolean;
+  quoteState: QuoteState;
+  rating: number;
+  experienceYears: number;
+  quoteCount: number;
+  isFavorite: boolean;
+  totalCustomerFavorite: number;
+  ratingCount: number;
+  price: number;
+  quoteId: string;
+  movingDate: Date;
+  departure: string;
+  arrival: string;
+  imageUrl: string;
+  onConfirmClick: () => void;
+  onDetailClick?: () => void;
+  onFavoriteClick: (quoteId: string) => void;
+  moverId: string;
+}
+
+export default function Page() {
+  const router = useRouter();
+  const [movers, setMovers] = useState<MoverInfoTemplateProps[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const itemsPerPage = 4;
+
+  useEffect(() => {
+    const checkLogin = async () => {
+      await axiosInstance.get(
+        '/auth/fakeSignIn?userId=cm8sc9j9q0012uzks94fmpzu5&roleId=cm8sc9jeg003wuzksdt8q4ngq&type=customer',
+      );
+      setIsLoggedIn(true);
+    };
+    checkLogin().catch(() => setIsLoggedIn(false));
+  }, []);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      const fetchPendingQuotes = async () => {
+        try {
+          const response = await axiosInstance.get('/quote/pending-quotes');
+          const data = response.data;
+          const quotes: Quote = data.data;
+          console.log('quotes:', quotes);
+          if (quotes && Array.isArray(quotes.moverQuotes)) {
+            const transformedMovers = quotes.moverQuotes.map(
+              (moverQuote: MoverQuote) => {
+                const movingTypes: ('small' | 'office' | 'home')[] =
+                  moverQuote.mover.moverServices.map((service) => {
+                    switch (service.serviceType) {
+                      case 'SMALL_MOVE':
+                        return 'small';
+                      case 'OFFICE_MOVE':
+                        return 'office';
+                      case 'HOME_MOVE':
+                        return 'home';
+                      default:
+                        return 'small';
+                    }
+                  });
+                return {
+                  variant: 'quote' as Variant,
+                  subVariant: 'pending' as SubVariant,
+                  moverName: moverQuote.mover.introduction,
+                  movingType: movingTypes,
+                  isCustomQuote: false,
+                  quoteState: 'pendingQuote' as QuoteState,
+                  rating: moverQuote.mover.averageRating,
+                  experienceYears: moverQuote.mover.experienceYears,
+                  quoteCount: moverQuote.mover.totalConfirmedCount,
+                  isFavorite: false,
+                  totalCustomerFavorite: moverQuote.mover.totalCustomerFavorite,
+                  ratingCount: moverQuote.mover.totalReviews,
+                  price: moverQuote.price,
+                  quoteId: moverQuote.id,
+                  movingDate: new Date(quotes.moveDate),
+                  departure: `${
+                    quotes.quoteRequestAddresses.find(
+                      (addr) => addr.type === 'DEPARTURE',
+                    )?.sido
+                  } ${
+                    quotes.quoteRequestAddresses.find(
+                      (addr) => addr.type === 'DEPARTURE',
+                    )?.sigungu
+                  }`,
+                  arrival: `${
+                    quotes.quoteRequestAddresses.find(
+                      (addr) => addr.type === 'ARRIVAL',
+                    )?.sido
+                  } ${
+                    quotes.quoteRequestAddresses.find(
+                      (addr) => addr.type === 'ARRIVAL',
+                    )?.sigungu
+                  }`,
+                  imageUrl: moverQuote.mover.profileImage,
+                  moverId: moverQuote.mover.id,
+                  onConfirmClick: () => handleConfirmClick(moverQuote.id),
+                  onDetailClick: () =>
+                    router.push(`/user/quotes/${moverQuote.id}`),
+                  onFavoriteClick: (moverId: string) =>
+                    handleFavoriteClick(moverId),
+                };
+              },
+            );
+            setMovers(transformedMovers);
+          }
+        } catch (error) {
+          console.error('Error fetching quotes:', error);
+        }
+      };
+      fetchPendingQuotes();
+    }
+  }, [isLoggedIn]);
+
+  const handleConfirmClick = async (quoteId: string) => {
+    try {
+      const response = await axiosInstance.post(
+        `/quote/confirm-quote/${quoteId}`,
+      );
+      if (response.status === 200) {
+        alert('견적이 확정되었습니다.');
+        setMovers((prevMovers) =>
+          prevMovers.filter((mover) => mover.quoteId !== quoteId),
+        );
+      }
+    } catch (error) {
+      console.error('Error confirming quote:', error);
+      alert('견적 확정에 실패했습니다.');
+    }
+  };
+
+  // 찜하기 버튼 클릭 시
+  const handleFavoriteClick = async (moverId: string) => {
+    console.log('하트 클릭 moverID:', moverId); // 디버깅
+    // 상태 업데이트 전 콘솔
+    console.log('movers1:', movers);
+    const updatedMovers = movers.map((mover) =>
+      mover.moverId === moverId
+        ? {
+            ...mover,
+            isFavorite: !mover.isFavorite,
+            totalCustomerFavorite: mover.isFavorite
+              ? mover.totalCustomerFavorite - 1
+              : mover.totalCustomerFavorite + 1,
+          }
+        : mover,
+    );
+    console.log('Updated movers:', updatedMovers);
+    const selectedMover = updatedMovers.find(
+      (mover) => mover.moverId === moverId,
+    );
+    console.log(updatedMovers);
+    if (!selectedMover) return;
+    console.log('b');
+    const apiEndpoint = selectedMover.isFavorite
+      ? `/favorites/create/${moverId}`
+      : `/favorites/delete/${moverId}`;
+    console.log(`Calling API endpoint: ${apiEndpoint}`); // 디버깅
+    try {
+      const response = await axiosInstance.post(apiEndpoint);
+      console.log('API response:', response); // 디버깅
+      if (response.status === 201 || response.status === 200) {
+        setMovers(updatedMovers);
+        console.log('상태 업데이트 후:', updatedMovers); // 디버깅
+      } else {
+        alert('찜하기 상태 변경에 실패했습니다.');
+      }
+    } catch (error) {
+      console.error('Error with favorite API:', error);
+      alert('찜하기 상태 변경에 실패했습니다.');
+    }
+  };
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentMovers = movers.slice(indexOfFirstItem, indexOfLastItem);
+
+  return (
+    <>
+      <div className='grid grid-cols-1 gap-6 py-[32px] max-w-[1400px] mx-auto xl:py-[40px] xl:grid-cols-2'>
+        {currentMovers.map((mover) => (
+          <MoverInfo
+            key={mover.quoteId}
+            {...mover}
+            imageUrl={mover.imageUrl}
+            favoriteCount={mover.totalCustomerFavorite ?? 0}
+            ratingCount={mover.ratingCount ?? 0}
+            onFavoriteClick={() => mover.onFavoriteClick(mover.moverId)}
+          />
+        ))}
+      </div>
+      <div className='flex justify-center items-center'>
+        <Pagination
+          currentPage={currentPage}
+          totalPages={Math.ceil(movers.length / itemsPerPage)}
+          onPageChange={(page) => setCurrentPage(page)}
+        />
+      </div>
+    </>
+  );
+}
