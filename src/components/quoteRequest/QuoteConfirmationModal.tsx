50f223c0cb943e89d5ba7995eec568642d68676f
강대원 <alexnori.com@gmail.com>
2025-04-01 13:47:15 +0900

    style: 견적 요청 확인 모달 UI 수정

--- src/components/quoteRequest/QuoteConfirmationModal.tsx
+++ src/components/quoteRequest/QuoteConfirmationModal.tsx
@@ -4,6 +4,9 @@ import combineDateTime from '@/utils/combineDateTime';
 import { maxStep } from './QuoteRequestPage';
 import { useCreateQuoteRequestMutation } from '@/hooks/useCreateQuoteRequestMutation';
 import { useRouter } from 'next/navigation';
+import formatKoreanDate from '@/utils/formatKoreanDate';
+import formatKoreanTime from '@/utils/formatKoreanTime';
+import MovingInfo from '../common/movingInfo/organisms/movingInfo';
 
 interface QuoteConfirmationModalProps {
   setShowModal: (value: boolean) => void;
@@ -43,7 +46,7 @@ export function QuoteConfirmationModal({
 
     setTimeout(() => {
       router.push('/user/quotes/requested');
-    }, 1000);
+    }, 500);
   };
 
   return (
@@ -52,8 +55,7 @@ export function QuoteConfirmationModal({
       onClose={() => setShowModal(false)}
     >
       <div className='mt-4'>
-        <p className='mb-2'>견적에 대한 정보:</p>
-
+        {/* <p className='mb-2'>견적에 대한 정보:</p> */}
         {/* <pre className='bg-gray-100 p-2 text-sm'>
           {JSON.stringify(
             {
@@ -67,15 +69,26 @@ export function QuoteConfirmationModal({
             2,
           )}
         </pre> */}
+        <MovingInfo
+          requestedDate={registerData.moveDate!} // 이사요청일 = 생성일 (임시)
+          movingType={registerData.moveType}
+          movingDate={
+            new Date(
+              combineDateTime(registerData.moveDate!, registerData.moveTime),
+            )
+          }
+          departure={registerData.moveFrom.fullAddress}
+          arrival={registerData.moveTo.fullAddress}
+        />
 
-        <p className='mt-4 text-xl text-red-500'>
+        <p className='mt-4 text-xl text-red-500 font-semibold'>
           주의: 견적은 수정을 못하니 잘못 적지 않았는지 확인 부탁드립니다!
         </p>
       </div>
       <div className='mt-6'>
         <button
           onClick={handleFinalConfirm}
-          className='w-full py-2 bg-blue-500 text-white rounded-lg font-bold hover:bg-blue-600 transition duration-200'
+          className='w-full py-4 text-xl bg-blue-500 text-white rounded-lg font-bold hover:bg-blue-600 transition duration-200'
         >
           견적 요청하기
         </button>
