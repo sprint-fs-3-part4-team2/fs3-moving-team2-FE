4feb75f496ed2fe88480425dab411d4bf44dbe6a
Jinhan <wlsgks333@naver.com>
2025-04-02 16:26:34 +0900

    fix: style 수정

--- src/components/authPage/layouts/authPageLayout/constants.ts
+++ src/components/authPage/layouts/authPageLayout/constants.ts
@@ -2,4 +2,4 @@ export const AUTH_PAGE_LAYOUT_STYLES =
   'flex items-start justify-center w-screen h-screen pt-[56px]';
 
 export const AUTH_PAGE_CONTENT_STYLES =
-  'flex flex-col items-center justify-start h-screen w-full';
+  'flex flex-col items-center justify-start w-full';
