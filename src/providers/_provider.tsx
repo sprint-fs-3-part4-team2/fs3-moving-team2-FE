ed257d0d86358d5eef892c7394a525bd87c0d428
heonq <gkagjsrb@naver.com>
2025-04-02 11:56:26 +0900

    feat: WarningProvider dynamic import 설정

--- src/providers/_provider.tsx
+++ src/providers/_provider.tsx
@@ -1,7 +1,10 @@
 import { PropsWithChildren } from 'react';
 import QueryProvider from '@/providers/queryProvider';
 import ToasterProvider from '@/providers/toastProvider';
-import WarningProvider from './warningProvider';
+import dynamic from 'next/dynamic';
+const WarningProvider = dynamic(() => import('./warningProvider'), {
+  ssr: false,
+});
 
 // provider 관련 코드 합치기
 export default function Provider({ children }: PropsWithChildren) {
