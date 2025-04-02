46038845e0ae1648f29e6782c75217e443b89002
heonq <gkagjsrb@naver.com>
2025-04-02 14:30:30 +0900

    style: 경고 메시지 오타 수정

--- src/constants/warningMessages.ts
+++ src/constants/warningMessages.ts
@@ -1,5 +1,5 @@
 // key에 쿼리가 될 값을 입력하고 value에 표시할 메시지를 입력해주세요.
 export const WARNING_MESSAGES = {
-  customerAccountExist: '알빈 유저 계정이 이미 존재합니다.',
+  customerAccountExist: '일반 유저 계정이 이미 존재합니다.',
   moverAccountExist: '기사님 계정이 이미 존재합니다.',
 } as const;
