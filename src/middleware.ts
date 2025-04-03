d24047068b8a06015b152baeeaf905bd71e4763b
kimhyunmook_mac <gusanr4200@naver.com>
2025-04-03 15:43:57 +0900

    .

--- src/middleware.ts
+++ src/middleware.ts
@@ -31,84 +31,85 @@ export function middleware(request: NextRequest) {
   const noAccess = '?warn=noAccess';
 
   // dev 환경에선 미들웨어 막기
-  // if (process.env.NODE_ENV === 'development') return res;
+  if (process.env.NODE_ENV === 'development') return res;
 
-  // if (process.env.NODE_ENV === 'production') {
-  // 비 로그인
-  if (!token) {
-    const nouserProtected =
-      PROTECT.NO_USER.some((path) => pathname.startsWith(path)) &&
-      !pathname.includes('sign-in') &&
-      !pathname.includes('sign-up');
+  if (process.env.NODE_ENV === 'production') {
+    // 비 로그인
+    if (!token) {
+      const nouserProtected =
+        PROTECT.NO_USER.some((path) => pathname.startsWith(path)) &&
+        !pathname.includes('sign-in') &&
+        !pathname.includes('sign-up');
 
-    if (nouserProtected) {
-      const loginUrl = new URL('/select-role' + authQuery, url);
-      return NextResponse.redirect(loginUrl);
-    }
-    return res;
-  }
-
-  // 로그인
-  if (token) {
-    const decode = jwtDecode(token) as CustomJWTPayload;
-    const referer = request.headers.get('referer');
-
-    if (!decode) return res;
-
-    const { roleId, type } = decode;
-
-    //프로필 미등록 유저 블럭
-    if (!roleId && !pathname.includes('/profile/register')) {
-      const referer = request.headers.get('referer');
-      const urlPath = `/${type === 'customer' ? 'user' : 'mover'}/profile/register`;
-
-      if (!referer) return NextResponse.redirect(new URL(urlPath, url));
-
-      if (referer.includes('/profile/register'))
-        return NextResponse.redirect(
-          new URL(urlPath + '?warn=profileRegister', url),
-        );
-
-      return NextResponse.redirect(new URL(urlPath, url));
-    }
-
-    // 프로필 등록 유저 블럭
-    if (roleId && pathname.includes('/profile/register')) {
-      if (type === 'customer') {
-        return NextResponse.redirect(
-          new URL('/user/quotes/request' + noAccess, url),
-        );
-      } else {
-        return NextResponse.redirect(
-          new URL('/mover/quotes/requested' + noAccess, url),
-        );
+      if (nouserProtected) {
+        const loginUrl = new URL('/select-role' + authQuery, url);
+        return NextResponse.redirect(loginUrl);
       }
+      return res;
     }
 
-    const protectType = type === 'customer' ? PROTECT.CUSTOMER : PROTECT.MOVER;
-    const authUserProtected = protectType.some((path) =>
-      pathname.includes(path),
-    );
+    // 로그인
+    if (token) {
+      const decode = jwtDecode(token) as CustomJWTPayload;
+      const referer = request.headers.get('referer');
 
-    if (
-      pathname.includes('sign-in') ||
-      pathname.includes('sign-up') ||
-      pathname.includes('select-role')
-    ) {
-      return NextResponse.redirect(new URL('/' + noAccess, url));
-    }
+      if (!decode) return res;
 
-    if (authUserProtected) {
-      if (referer) {
-        return NextResponse.redirect(new URL(referer + noAccess, url));
-      } else {
+      const { roleId, type } = decode;
+
+      //프로필 미등록 유저 블럭
+      if (!roleId && !pathname.includes('/profile/register')) {
+        const referer = request.headers.get('referer');
+        const urlPath = `/${type === 'customer' ? 'user' : 'mover'}/profile/register`;
+
+        if (!referer) return NextResponse.redirect(new URL(urlPath, url));
+
+        if (referer.includes('/profile/register'))
+          return NextResponse.redirect(
+            new URL(urlPath + '?warn=profileRegister', url),
+          );
+
+        return NextResponse.redirect(new URL(urlPath, url));
+      }
+
+      // 프로필 등록 유저 블럭
+      if (roleId && pathname.includes('/profile/register')) {
+        if (type === 'customer') {
+          return NextResponse.redirect(
+            new URL('/user/quotes/request' + noAccess, url),
+          );
+        } else {
+          return NextResponse.redirect(
+            new URL('/mover/quotes/requested' + noAccess, url),
+          );
+        }
+      }
+
+      const protectType =
+        type === 'customer' ? PROTECT.CUSTOMER : PROTECT.MOVER;
+      const authUserProtected = protectType.some((path) =>
+        pathname.includes(path),
+      );
+
+      if (
+        pathname.includes('sign-in') ||
+        pathname.includes('sign-up') ||
+        pathname.includes('select-role')
+      ) {
         return NextResponse.redirect(new URL('/' + noAccess, url));
       }
-    }
-  }
 
-  return res;
-  // }
+      if (authUserProtected) {
+        if (referer) {
+          return NextResponse.redirect(new URL(referer + noAccess, url));
+        } else {
+          return NextResponse.redirect(new URL('/' + noAccess, url));
+        }
+      }
+    }
+
+    return res;
+  }
 }
 
 interface CustomJWTPayload {
