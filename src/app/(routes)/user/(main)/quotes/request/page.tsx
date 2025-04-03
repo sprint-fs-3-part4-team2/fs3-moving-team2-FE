7ee072a85eda9f0aa941ce87c8bb06833b964388
강대원 <alexnori.com@gmail.com>
2025-04-03 17:01:54 +0900

    fix: ssr->csr로 변경

--- src/app/(routes)/user/(main)/quotes/request/page.tsx
+++ src/app/(routes)/user/(main)/quotes/request/page.tsx
@@ -1,22 +1,16 @@
-export const dynamic = 'force-dynamic';
-
+'use client';
 import QuoteRequestPage from '@/components/quoteRequest/QuoteRequestPage';
 import QuoteRequestInProgressPage from '@/components/quoteRequest/QuoteRequestInProgressPage';
-import { getlatestQuoteRequest } from '@/services/quoteRequests';
+import { getQuoteRequest } from '@/services/quoteRequests';
+import { useQuery } from '@tanstack/react-query';
 
 export default async function Page() {
-  let data = null;
-  try {
-    data = await getlatestQuoteRequest();
-    console.log('getlatestQuoteRequest', data);
-  } catch (error) {
-    console.error('Error fetching : getlatestQuoteRequest', error);
-    return (
-      <div>데이터를 불러오는 데 실패했습니다. 잠시 후 다시 시도해주세요.</div>
-    );
-  }
+  const { data } = useQuery({
+    queryKey: ['myQuoteRequest'],
+    queryFn: getQuoteRequest,
+  });
 
-  return data.isRequested ? (
+  return data?.isRequested ? (
     <QuoteRequestInProgressPage />
   ) : (
     <QuoteRequestPage />
