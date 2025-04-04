be62e81d729ab2d82f20c77a4821436f3b5768fd
Jinhan <wlsgks333@naver.com>
2025-04-04 15:42:48 +0900

    chore: 테스트용 링크 추가

--- src/components/layout/gnb/template/index.tsx
+++ src/components/layout/gnb/template/index.tsx
@@ -7,6 +7,8 @@ import GNBMenu from '../molecules/gnbMenu';
 import { GNB_LOGO_MENU_STYLES, GNB_STYLES } from './constant';
 import dynamic from 'next/dynamic';
 import useUserProfile from '@/hooks/auth/useUserProfile';
+import CustomLink from '../atoms/menus/common/customLink';
+import Text from '../atoms/menus/common/gnbText';
 const GNBRightSection = dynamic(() => import('../organisms/gnbRightSection'), {
   ssr: false,
 });
@@ -14,6 +16,7 @@ const GNBRightSection = dynamic(() => import('../organisms/gnbRightSection'), {
 export default function GNB() {
   const pathname = usePathname();
   const { data } = useUserProfile();
+  console.log('user data: ', data);
 
   const isAuthorized = data ? true : false;
   const userName = data?.name || '';
@@ -39,6 +42,32 @@ export default function GNB() {
           <GNBLogo isAuthorized={isAuthorized} />
           <GNBMenu userType={userType} />
         </div>
+        {/* 테스트 페이지 이동 */}
+        <CustomLink href='/user/sign-in'>
+          <Text linkHref='/user/sign-in'>로그인</Text>
+        </CustomLink>
+
+        <CustomLink href='/user/sign-up'>
+          <Text linkHref='/user/sign-up'>회원가입</Text>
+        </CustomLink>
+
+        <div className='flex flex-col'>
+          <CustomLink href='/user/profile/register'>
+            <Text linkHref='/user/profile/register'>일반 프로필 등록</Text>
+          </CustomLink>
+          <CustomLink href='/mover/profile/register'>
+            <Text linkHref='/mover/profile/register'>기사님 프로필 등록</Text>
+          </CustomLink>
+        </div>
+
+        <CustomLink href='/mover/profile/edit'>
+          <Text linkHref='/mover/profile/edit'>기사님 프로필 수정</Text>
+        </CustomLink>
+
+        <CustomLink href='/mover/profile'>
+          <Text linkHref='/mover/profile'>기사님 마이페이지</Text>
+        </CustomLink>
+
         <GNBRightSection
           isAuthorized={isAuthorized}
           userName={userName}
