2e381e37636dc5b425aeb4b097637a83f590e093
jonghun4 <xwhdgns@gmail.com>
2025-04-04 16:44:07 +0900

    test: build error test2

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
