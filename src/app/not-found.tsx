74caf26307a2f4c720b91e45eb2c1d7e30a9faf2
yousuk88 <179006262+yousuk88@users.noreply.github.com>
2025-04-07 13:53:06 +0900

    style: 업데이트 404 not-found 이미지

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -35,7 +35,7 @@ export default function NotFound() {
 
   return (
     <div className='relative overflow-hidden w-full h-screen flex items-center justify-center'>
-      <div className='absolute w-full xl:w-auto top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center'>
+{/*       <div className='absolute w-full xl:w-auto top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center'>
         <Image
           className={''}
           src={moving}
@@ -52,7 +52,28 @@ export default function NotFound() {
             존재하지 않는 페이지입니다.
           </p>
         </div>
-      </div>
+      </div> */}
+      <div className='group relative w-[214px] md:w-[321px] xl:w-[428px] h-[240.4px] md:h-[360.6px] xl:h-[480.8px]'>
+          <Image
+            src="/img/no-data/404.png"
+            alt="not_found"
+            fill
+            priority
+            style={{ objectFit: "cover" }}
+            className="block group-hover:hidden"
+          />
+          <Image
+            src="/img/no-data/404_empty.png"
+            alt="not_found_hover"
+            fill
+            priority
+            style={{ objectFit: "cover" }}
+            className="hidden group-hover:block"
+          />
+        </div>
+        <div className='pt-[24px] xl:pt-[32px] text-grayscale-400 text-[16px] xl:text-[24px]'>
+          존재하지 않는 페이지입니다
+        </div>
     </div>
   );
 }
