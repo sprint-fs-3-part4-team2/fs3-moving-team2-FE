46d2efa80e7d925fbd86270ee7d4574c4a1977d8
yousuk88 <179006262+yousuk88@users.noreply.github.com>
2025-04-07 14:28:38 +0900

    업데이트 404 not-found 이미지

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -35,8 +35,8 @@ export default function NotFound() {
 
   return (
     <div className='relative overflow-hidden w-full h-screen flex items-center justify-center'>
-{/*       <div className='absolute w-full xl:w-auto top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center'>
-        <Image
+      <div className='absolute w-full xl:w-auto top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center'>
+{/*         <Image
           className={''}
           src={moving}
           alt='무빙 트럭'
@@ -51,9 +51,8 @@ export default function NotFound() {
           <p className='text-grayscale-300 text-center text-2xl xl:text-xl mt-6 break-keep'>
             존재하지 않는 페이지입니다.
           </p>
-        </div>
-      </div> */}
-      <div className='group relative w-[214px] md:w-[321px] xl:w-[428px] h-[240.4px] md:h-[360.6px] xl:h-[480.8px]'>
+        </div> */}
+        <div className='group relative w-[214px] md:w-[321px] xl:w-[428px] h-[240.4px] md:h-[360.6px] xl:h-[480.8px]'>
           <Image
             src="/img/no-data/404.png"
             alt="not_found"
@@ -74,6 +73,7 @@ export default function NotFound() {
         <div className='pt-[24px] xl:pt-[32px] text-grayscale-400 text-[16px] xl:text-[24px]'>
           존재하지 않는 페이지입니다
         </div>
+      </div>
     </div>
   );
 }
