63a88978df611fee0ffb4cea81aa522f0f335944
heonq <gkagjsrb@naver.com>
2025-04-08 18:34:52 +0900

    feat: Loading페이지, 데이터 없음 페이지 공통 컴포넌트 적용

--- src/app/(routes)/mover/(main)/quotes/(myQuotes)/submitted/page.tsx
+++ src/app/(routes)/mover/(main)/quotes/(myQuotes)/submitted/page.tsx
@@ -1,10 +1,11 @@
+import Loading from '@/app/loading';
 import SubmittedQuotesByMover from '@/components/submittedQuotesByMover';
 import { Suspense } from 'react';
 
 export default function Page() {
   return (
     <div className='flex w-full bg-backgroundVariants-100 flex-1 py-10'>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Loading />}>
         <SubmittedQuotesByMover />
       </Suspense>
     </div>
