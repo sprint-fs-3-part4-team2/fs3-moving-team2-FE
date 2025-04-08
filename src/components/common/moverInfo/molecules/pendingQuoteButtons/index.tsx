a379a9fbcb2cec17ddac162cb4a900660350f108
heonq <gkagjsrb@naver.com>
2025-04-08 14:44:08 +0900

    style: pendingQuteButtons 스타일 수정

--- src/components/common/moverInfo/molecules/pendingQuoteButtons/index.tsx
+++ src/components/common/moverInfo/molecules/pendingQuoteButtons/index.tsx
@@ -1,28 +1,25 @@
 import CommonButton from '@/components/common/commonBtn/commonBtn';
 import { PendingQuoteButtonsProps } from './pendingQuoteButtons.types';
-import { PENDING_QUOTE_BUTTON_STYLE } from './constants';
 
 export default function PendingQuoteButtons({
   onConfirmClick,
   onDetailClick,
 }: PendingQuoteButtonsProps) {
   return (
-    <div className='flex flex-col md:flex-row xl:flex-row justify-between items-center gap-[8px] md:gap-[8px] xl:gap-[11px] xl:text-xl'>
+    <div className='flex flex-col md:flex-row xl:flex-row justify-between items-center gap-[8px] md:gap-[8px] xl:gap-[11px] xl:text-xl W-full'>
       <CommonButton
-        widthType='dynamic'
+        widthType='full'
         heightType='primary'
-        className={PENDING_QUOTE_BUTTON_STYLE}
         onClick={onConfirmClick}
       >
         견적 확정하기
       </CommonButton>
       <CommonButton
-        widthType='dynamic'
+        widthType='full'
         heightType='primary'
         backgroundColorType='white'
         borderColorsType='blue'
         textColorType='blue'
-        className={PENDING_QUOTE_BUTTON_STYLE}
         onClick={onDetailClick}
       >
         상세보기
