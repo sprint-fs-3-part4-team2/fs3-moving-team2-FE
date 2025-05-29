1b594a07f37fb9e9fca7c2f87c426378115dda7a
강대원 <alexnori.com@gmail.com>
2025-05-29 16:35:03 +0900

    refactor: 전통적 isLoading / isError 핸들링 방식 적용

--- src/app/(routes)/user/(main)/quotes/request/page.tsx
+++ src/app/(routes)/user/(main)/quotes/request/page.tsx
@@ -6,7 +6,7 @@ import { useQuery } from '@tanstack/react-query';
 import Loading from '@/app/loading';
 
 export default function Page() {
-  const { data, isLoading } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: ['myQuoteRequest'],
     queryFn: getQuoteRequest,
   });
@@ -15,6 +15,16 @@ export default function Page() {
     return (
       <div className='flex justify-center items-center w-full h-screen'>
         <Loading />
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className='flex justify-center items-center w-full h-screen'>
+        <p className='text-red-500'>
+          견적 요청 정보를 불러오는 데 실패했습니다. 나중에 다시 시도해주세요.
+        </p>
       </div>
     );
   }
